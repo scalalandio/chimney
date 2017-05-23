d55859248b034c7d0fed2725a3aa1d9c4635f966
Piotr Krzemiński <pio.krzeminski@gmail.com>
2017-05-24 01:15:07 +0200

    support for value classes

--- chimney/src/test/scala/io/scalaland/chimney/DslSpec.scala
+++ chimney/src/test/scala/io/scalaland/chimney/DslSpec.scala
@@ -5,13 +5,6 @@ import shapeless.test._
 
 class DslSpec extends WordSpec with MustMatchers {
 
-  case class UserName(value: String)
-
-  val userNameToStringTransformer: Transformer[UserName, String] =
-    (_: UserName).value
-
-  case class UserDTO(id: String, name: String)
-  case class User(id: String, name: UserName)
 
   import dsl._
 
@@ -19,18 +12,22 @@ class DslSpec extends WordSpec with MustMatchers {
 
     "use implicit transformer directly" in {
 
+      import Domain1._
+
       implicit val _ = userNameToStringTransformer
 
-      UserName("Batman").into[String].transform mustBe "Batman"
+      UserName("Batman").transformInto[String] mustBe "Batman"
       UserName("Batman").transformInto[String] mustBe "Batman"
     }
 
     "use implicit transformer for nested field" in {
 
+      import Domain1._
+
       implicit val _ = userNameToStringTransformer
 
       val batman = User("123", UserName("Batman"))
-      val batmanDTO = batman.into[UserDTO].transform
+      val batmanDTO = batman.transformInto[UserDTO]
 
       batmanDTO.id mustBe batman.id
       batmanDTO.name mustBe batman.name.value
@@ -42,14 +39,14 @@ class DslSpec extends WordSpec with MustMatchers {
       case class Bar(x: Int, z: Double)
 
       "field is dropped in the target" in {
-        Foo(3, "pi", 3.14).into[Bar].transform mustBe Bar(3, 3.14)
+        Foo(3, "pi", 3.14).transformInto[Bar] mustBe Bar(3, 3.14)
       }
 
       "field is added to the target" should {
 
         "not compile if source for the target fields is not provided" in {
 
-          illTyped("Bar(3, 3.14).into[Foo].transform")
+          illTyped("Bar(3, 3.14).transformInto[Foo]")
         }
 
         "fill the field with provided default value" in {
@@ -79,7 +76,7 @@ class DslSpec extends WordSpec with MustMatchers {
 
       "not compile if relabelling modifier is not provided" in {
 
-        illTyped("""Foo(10, "something").into[Bar].transform""")
+        illTyped("""Foo(10, "something").transformInto[Bar]""")
       }
 
       "relabel fields with relabelling modifier" in {
@@ -101,5 +98,43 @@ class DslSpec extends WordSpec with MustMatchers {
         )
       }
     }
+
+    "support value classes" when {
+
+      import VCDomain1._
+
+      "transforming value class to a value" in {
+
+        UserName("Batman").transformInto[String] mustBe "Batman"
+        User("100", UserName("abc")).transformInto[UserDTO] mustBe
+          UserDTO("100", "abc")
+      }
+
+      "transforming value to a value class" in {
+
+        "Batman".transformInto[UserName] mustBe UserName("Batman")
+        UserDTO("100", "abc").transformInto[User] mustBe
+          User("100", UserName("abc"))
+
+      }
+    }
   }
+}
+
+object Domain1 {
+
+  case class UserName(value: String)
+
+  val userNameToStringTransformer: Transformer[UserName, String] =
+    (_: UserName).value
+
+  case class UserDTO(id: String, name: String)
+  case class User(id: String, name: UserName)
+}
+
+object VCDomain1 {
+
+  case class UserName(value: String) extends AnyVal
+  case class UserDTO(id: String, name: String)
+  case class User(id: String, name: UserName)
 }
