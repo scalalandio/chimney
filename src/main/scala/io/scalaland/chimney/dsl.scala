81ea14668be23df91e6449fa37e548fa63468072
Piotr Krzemiński <pio.krzeminski@gmail.com>
2017-05-23 17:29:52 +0200

    prototyping transformer modifiers for fields

--- src/main/scala/io/scalaland/chimney/dsl.scala
+++ src/main/scala/io/scalaland/chimney/dsl.scala
@@ -4,8 +4,21 @@ object dsl {
 
   implicit class TransformerOps[From](val source: From) extends AnyVal {
 
-    def transformTo[To](implicit transformer: Transformer[From, To]): To =
-      transformer.transform(source)
+    def transformer[To]: TransformTo[From, To] = new TransformTo(source)
   }
+
+  class TransformTo[From, To](val source: From) extends AnyVal {
+
+    def invoke(implicit transformer: Transformer.Aux[From, To]): To =
+      transformer.transform(source, Modifier.empty)
+
+    def invokeM[M <: Modifier](modifier: M)
+                              (implicit transformer: Transformer[From, To, M]): To =
+      transformer.transform(source, modifier)
+  }
+
+  type Modifier = io.scalaland.chimney.Modifier
+  val Modifier = io.scalaland.chimney.Modifier
+
 
 }
