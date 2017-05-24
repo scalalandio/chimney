f650adc97646aa4fb5cf6132acb3625c51653aa1
Piotr Krzemiński <pio.krzeminski@gmail.com>
2017-05-24 12:09:20 +0200

    cosmetics

--- chimney/src/main/scala/io/scalaland/chimney/DerivedTransformer.scala
+++ chimney/src/main/scala/io/scalaland/chimney/DerivedTransformer.scala
@@ -85,13 +85,15 @@ trait EitherInstances {
 }
 
 trait CollectionInstances {
+
   implicit def traversableTransformer[From, To, Modifiers <: HList, M[_]]
     (implicit innerTransformer: DerivedTransformer[From, To, Modifiers],
      ev1: M[From] <:< Traversable[From],
      ev2: M[To] <:< Traversable[To],
      cbf: CanBuildFrom[M[From], To, M[To]])
   : DerivedTransformer[M[From], M[To], Modifiers] =
-    (src: M[From], modifiers: Modifiers) => src.map(innerTransformer.transform(_: From, modifiers)).to[M]
+    (src: M[From], modifiers: Modifiers) =>
+      src.map(innerTransformer.transform(_: From, modifiers)).to[M]
 
   implicit def arrayTransformer[From, To, Modifiers <: HList]
     (implicit innerTransformer: DerivedTransformer[From, To, Modifiers], toTag: ClassTag[To])
