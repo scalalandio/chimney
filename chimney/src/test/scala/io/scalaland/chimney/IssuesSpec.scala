23e074c1de28660e638d1c71fe3bd3f31cdc3aba
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2017-06-01 17:47:31 +0200

    Added test case

--- chimney/src/test/scala/io/scalaland/chimney/IssuesSpec.scala
+++ chimney/src/test/scala/io/scalaland/chimney/IssuesSpec.scala
@@ -2,7 +2,6 @@ package io.scalaland.chimney
 
 import org.scalatest.{MustMatchers, WordSpec}
 
-
 class IssuesSpec extends WordSpec with MustMatchers {
 
   import dsl._
@@ -18,6 +17,17 @@ class IssuesSpec extends WordSpec with MustMatchers {
         .withFieldConst('isDeleted, false)
         .transform mustBe
         Entity(0, "name", isDeleted = false)
+    }
+
+    "fix issue #21" in {
+      import shapeless.tag
+      import shapeless.tag._
+      sealed trait Test
+
+      case class EntityWithTag1(id: Long, name: String @@ Test)
+      case class EntityWithTag2(id: Long, name: String @@ Test)
+
+      EntityWithTag1(0L, tag[Test]("name")).transformInto[EntityWithTag2] mustBe EntityWithTag2(0L, tag[Test]("name"))
     }
   }
 
