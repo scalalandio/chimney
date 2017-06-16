dd04f069f61e16aa378f64a59fff12a71b955218
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2017-06-16 14:41:32 +0200

    fixup! Moved internal classes and implementations to a dedicated package

--- chimney/src/main/scala/io/scalaland/chimney/dsl.scala
+++ chimney/src/main/scala/io/scalaland/chimney/dsl.scala
@@ -22,20 +22,20 @@ object dsl {
       withFieldComputed(label, _ => value)
 
     def withFieldComputed[T](
-                              label: Witness.Lt[Symbol],
-                              map: From => T
-                            ): TransformerInto[From, To, Modifier.fieldFunction[label.T, From, T] :: Modifiers] =
+      label: Witness.Lt[Symbol],
+      map: From => T
+    ): TransformerInto[From, To, Modifier.fieldFunction[label.T, From, T] :: Modifiers] =
       new TransformerInto(source, new Modifier.fieldFunction[label.T, From, T](map) :: modifiers)
 
     def withFieldRenamed(
-                          labelFrom: Witness.Lt[Symbol],
-                          labelTo: Witness.Lt[Symbol]
-                        ): TransformerInto[From, To, Modifier.relabel[labelFrom.T, labelTo.T] :: Modifiers] =
+      labelFrom: Witness.Lt[Symbol],
+      labelTo: Witness.Lt[Symbol]
+    ): TransformerInto[From, To, Modifier.relabel[labelFrom.T, labelTo.T] :: Modifiers] =
       new TransformerInto(source, new Modifier.relabel[labelFrom.T, labelTo.T] :: modifiers)
 
     def withCoproductInstance[Inst](
-                                     f: Inst => To
-                                   ): TransformerInto[From, To, Modifier.coproductInstance[Inst, To] :: Modifiers] =
+      f: Inst => To
+    ): TransformerInto[From, To, Modifier.coproductInstance[Inst, To] :: Modifiers] =
       new TransformerInto(source, new Modifier.coproductInstance[Inst, To](f) :: modifiers)
 
     def transform(implicit transformer: DerivedTransformer[From, To, Modifiers]): To =
