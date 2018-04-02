f98fb74167f1644ea85cae57fe9b8398d5fd26e5
Piotr Krzemiński <pio.krzeminski@gmail.com>
2018-04-02 23:12:02 +0200

    fix formatting

--- chimney/src/main/scala/io/scalaland/chimney/dsl.scala
+++ chimney/src/main/scala/io/scalaland/chimney/dsl.scala
@@ -13,7 +13,9 @@ object dsl {
     final def into[To]: TransformerInto[From, To, Modifier.enableDefaultValues :: HNil] =
       new TransformerInto(source, Modifier.enableDefaultValues :: HNil)
 
-    final def transformInto[To](implicit derivedTransformer: DerivedTransformer[From, To, Modifier.enableDefaultValues :: HNil]): To =
+    final def transformInto[To](
+      implicit derivedTransformer: DerivedTransformer[From, To, Modifier.enableDefaultValues :: HNil]
+    ): To =
       derivedTransformer.transform(source, Modifier.enableDefaultValues :: HNil)
   }
 
@@ -58,7 +60,9 @@ object dsl {
     ): TransformerInto[From, To, Modifier.coproductInstance[Inst, To] :: Modifiers] =
       new TransformerInto(source, new Modifier.coproductInstance[Inst, To](f) :: modifiers)
 
-    def disableDefaultValues[Modifiers2 <: HList](implicit filterNot: FilterNot.Aux[Modifiers, Modifier.enableDefaultValues, Modifiers2]): TransformerInto[From, To, Modifiers2] =
+    def disableDefaultValues[Modifiers2 <: HList](
+      implicit filterNot: FilterNot.Aux[Modifiers, Modifier.enableDefaultValues, Modifiers2]
+    ): TransformerInto[From, To, Modifiers2] =
       new TransformerInto(source, filterNot(modifiers))
 
     def transform(implicit transformer: DerivedTransformer[From, To, Modifiers]): To =
