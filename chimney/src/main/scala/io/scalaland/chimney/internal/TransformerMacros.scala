f13f3209adacb88a5b80db4d640c41cdc0e1a985
Piotr Krzemiński <pio.krzeminski@gmail.com>
2018-04-15 17:15:05 +0200

    macro structure refactor

--- chimney/src/main/scala/io/scalaland/chimney/internal/TransformerMacros.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/TransformerMacros.scala
@@ -11,33 +11,53 @@ private[chimney] class TransformerMacros(val c: scala.reflect.macros.blackbox.Co
 
     println(s"XXXXX: $From ~~~~> $To")
 
-    val FromSymbol = From.typeSymbol
-    val ToSymbol = To.typeSymbol
+    val srcName = c.internal.reificationSupport.freshTermName(From.typeSymbol.name.decodedName.toString.toLowerCase + "$")
+    val srcPrefixTree = Ident(TermName(srcName.decodedName.toString))
 
-    (FromSymbol.classSymbolOpt, ToSymbol.classSymbolOpt) match {
-      case (Some(fromClassType), Some(toClassType)) if fromClassType.isCaseClass && toClassType.isCaseClass =>
+    expandTransformerTree(srcPrefixTree)(From, To) match {
 
-        println("Transforming between case classes!!!")
-
-        val srcName = c.internal.reificationSupport.freshTermName(fromClassType.name.decodedName.toString.toLowerCase + "$")
-        val srcPrefix = Ident(TermName(srcName.decodedName.toString))
-        val transformingTree = expandCaseClassTransformingExpr(srcPrefix)(From, To)
+      case Right(transformerTree) =>
         val tree = q"""
            new _root_.io.scalaland.chimney.Transformer[$From, $To] {
-             def transform($srcName: $From): $To = $transformingTree
+             def transform($srcName: $From): $To = $transformerTree
            }
         """
-
         println(s"FINAL TREE:\n$tree")
 
         c.Expr[io.scalaland.chimney.Transformer[From, To]](tree)
 
-      case (from, to) =>
-        c.abort(c.enclosingPosition, s"Unsupported case yet: $from ~~> $to")
+      case Left(derivationErrors) =>
+
+        val errorMessage =
+          s"""Chimney can't derive transformation from $From to $To
+             |
+             |${printDerivationErrors(derivationErrors)}
+             |See $chimneyDocUrl for usage examples.
+             |
+             |""".stripMargin
+
+        c.abort(c.enclosingPosition, errorMessage)
     }
   }
 
-  def expandCaseClassTransformingExpr(srcPrefix: Tree)(From: Type, To: Type): c.Tree = {
+  def expandTransformerTree(srcPrefixTree: Tree)(From: Type, To: Type): Either[Seq[ChimneyDerivationError], Tree] = {
+
+    var errors = Seq.empty[ChimneyDerivationError]
+
+    if(From.isCaseClass && To.isCaseClass) {
+      expandCaseClassTransformerTree(srcPrefixTree)(From, To)
+    } else {
+      Left(
+        Seq(
+          NotSupportedDerivation(From.typeSymbol.name.toString, To.typeSymbol.name.toString)
+        )
+      )
+    }
+  }
+
+  def expandCaseClassTransformerTree(srcPrefixTree: Tree)(From: Type, To: Type): Either[Seq[ChimneyDerivationError], Tree] = {
+
+    var errors = Seq.empty[ChimneyDerivationError]
 
     val fromParams = From.caseClassParams
     val toParams = To.caseClassParams
@@ -54,71 +74,71 @@ private[chimney] class TransformerMacros(val c: scala.reflect.macros.blackbox.Co
     val missingFields = mapping.collect { case (field, None) => field }
 
     if(missingFields.nonEmpty) {
-
-      def missingFieldLine(ms: MethodSymbol): String =
-        s"  ${ms.name}: ${ms.returnType} - no field named ${ms.name} in source type $From"
-
-      val errorMessage =
-        s"""Chimney can't find a data source for the following fields:
-           |$To
-           |${missingFields.map(missingFieldLine).mkString("\n")}
-           |
-           |Try adding .withFieldConst, .withFieldComputed or .withFieldRenamed. See more examples at $chimneyDocUrl
-           |
-           |""".stripMargin
-
-      c.abort(c.enclosingPosition, errorMessage)
-
-    } else {
-
-      val args = mapping.collect {
-        case (targetField, Some(sourceField)) if sourceField.returnType <:< targetField.returnType =>
-          // we know field types are the same
-          // TODO: we can be less strict and require only sourceField.returnType <:< targetField.returnType
-          q"$srcPrefix.${sourceField.name}"
-
-        case (targetField, Some(sourceField)) =>
-
-          println("SRCRET: " + sourceField.returnType + "  TARGRET: " + targetField.returnType)
-
-          findLocalImplicitTransformer(sourceField.returnType, targetField.returnType) match {
-            case Some(localImplicitTransformer) =>
-
-              q"$localImplicitTransformer.transform($srcPrefix.${sourceField.name})"
-
-            case None if sourceField.returnType.isCaseClass && targetField.returnType.isCaseClass =>
-
-              println("Both case classes, so GOOO!!!")
-
-              expandCaseClassTransformingExpr(q"$srcPrefix.${sourceField.name}")(sourceField.returnType, targetField.returnType)
-
-            case None =>
-
-              c.abort(c.enclosingPosition, "!!!")
-          }
+      missingFields.foreach { ms =>
+        errors :+= MissingField(
+          fieldName = ms.name.toString,
+          fieldTypeName = ms.returnType.toString,
+          sourceTypeName = From.toString,
+          targetTypeName = To.toString
+        )
       }
+    }
 
-      q"new $To(..$args)".debug
+    val args = mapping.collect {
+      case (targetField, Some(sourceField)) if sourceField.returnType <:< targetField.returnType =>
+        q"$srcPrefixTree.${sourceField.name}"
+
+      case (targetField, Some(sourceField)) =>
+
+        println("SRCRET: " + sourceField.returnType + "  TARGRET: " + targetField.returnType)
+
+        findLocalImplicitTransformer(sourceField.returnType, targetField.returnType) match {
+          case Some(localImplicitTransformer) =>
+
+            q"$localImplicitTransformer.transform($srcPrefixTree.${sourceField.name})"
+
+          case None if sourceField.returnType.isCaseClass && targetField.returnType.isCaseClass =>
+
+            expandTransformerTree(q"$srcPrefixTree.${sourceField.name}")(sourceField.returnType, targetField.returnType) match {
+              case Left(errs) =>
+                errors ++= errs
+                EmptyTree
+              case Right(tree) =>
+                tree
+            }
+
+          case None =>
+
+            errors :+= MissingTransformer(
+              fieldName = targetField.name.toString,
+              sourceFieldTypeName = sourceField.returnType.toString,
+              targetFieldTypeName = targetField.returnType.toString,
+              sourceTypeName = From.toString,
+              targetTypeName = To.toString
+            )
+
+            EmptyTree
+        }
+    }
+
+    if(errors.nonEmpty) {
+      Left(errors)
+    } else {
+      Right(q"new $To(..$args)".debug)
     }
   }
 
-  def findLocalImplicitTransformer(From: Type, To: Type): Option[Tree] = {
-    println(s"From = $From, To = $To")
 
+  def findLocalImplicitTransformer(From: Type, To: Type): Option[Tree] = {
     val tpeTree = c.typecheck(tree = tq"_root_.io.scalaland.chimney.Transformer[$From, $To]",
       silent = false,
       mode = c.TYPEmode,
       withImplicitViewsDisabled = true,
       withMacrosDisabled = true)
 
-
-    println(tpeTree + " ||| " + tpeTree.tpe)
-
-    val res = scala.util.Try(c.inferImplicitValue(tpeTree.tpe, withMacrosDisabled = true))
-    println(res)
-    val res2 = res.toOption.filterNot(_ == EmptyTree)
-    println(res2)
-    res2
+    scala.util.Try(c.inferImplicitValue(tpeTree.tpe, withMacrosDisabled = true))
+      .toOption
+      .filterNot(_ == EmptyTree)
   }
 
   private val primitives = Set(
@@ -164,4 +184,46 @@ private[chimney] class TransformerMacros(val c: scala.reflect.macros.blackbox.Co
   }
 
   private val chimneyDocUrl = "http://scalalandio.github.io/chimney"
+
+
+  sealed trait ChimneyDerivationError {
+    def sourceTypeName: String
+    def targetTypeName: String
+  }
+
+  case class MissingField(fieldName: String,
+                          fieldTypeName: String,
+                          sourceTypeName: String,
+                          targetTypeName: String) extends ChimneyDerivationError
+
+  case class MissingTransformer(fieldName: String,
+                                sourceFieldTypeName: String,
+                                targetFieldTypeName: String,
+                                sourceTypeName: String,
+                                targetTypeName: String) extends ChimneyDerivationError
+
+  case class NotSupportedDerivation(sourceTypeName: String,
+                                    targetTypeName: String) extends ChimneyDerivationError
+
+  private def printDerivationErrors(errors: Seq[ChimneyDerivationError]): String = {
+
+    errors
+      .groupBy(_.targetTypeName)
+      .map { case (targetTypeName, errs) =>
+
+        val errStrings = errs.map {
+          case MissingField(fieldName, fieldTypeName, sourceTypeName, _) =>
+            s"  $fieldName: $fieldTypeName - no field named $fieldName in source type $sourceTypeName"
+          case MissingTransformer(fieldName, sourceFieldTypeName, targetFieldTypeName, sourceTypeName, _) =>
+            s"  $fieldName: $targetFieldTypeName - can't derive transformation from $fieldName: $sourceFieldTypeName in source type $sourceTypeName"
+          case NotSupportedDerivation(sourceTypeName, _) =>
+            s"  derivation from $sourceTypeName is not supported in Chimney!"
+        }
+
+        s"""$targetTypeName
+           |${errStrings.mkString("\n")}
+           |""".stripMargin
+      }
+      .mkString("\n")
+  }
 }
--- chimney/src/test/scala/io/scalaland/chimney/Test.scala
+++ chimney/src/test/scala/io/scalaland/chimney/Test.scala
@@ -3,12 +3,11 @@ package io.scalaland.chimney
 import scala.language.experimental.macros
 
 case class Source(zzz: Int)
-case class Target(zzz: Int)
+case class Target(zzz: Int, jjj: String)
 
-case class Foo(x: Int, z: Boolean, y: String, abc: Source, u: Int, zzz: RuntimeException, kukuku: Unit)
+case class Foo(x: Int, z: Boolean, y: String, abc: Source, u: Int, zzz: RuntimeException, kukuku: Unit, lululu: Char, kaka: Float)
 case class Bar1(y: String, x: Int, abc: Target, u: String, zzz: Exception)
-case class Bar2(y: String, lululu: Unit, x: Int, abc: Target, u: String)
-
+case class Bar2(y: String, lululu: Unit, x: Int, abc: Target, u: String, kaka: String)
 
 
 object Test extends App {
@@ -17,11 +16,11 @@ object Test extends App {
 
   val t = Transformer.gen[Foo, Bar1]
 
-  val u = t.transform(Foo(10, false, "abc", Source(9999), 0, new RuntimeException,  ()))
-
-  //val yyy = Test.this.intToStringTransformer.transform(4)
+  val u = t.transform(Foo(10, false, "abc", Source(9999), 0, new RuntimeException,  (), 'x', 2.0f))
 
   println(u)
 
-  val t2 = Transformer.gen[Foo, Bar2]
+//  val t2 = Transformer.gen[Foo, Bar2]
+
+  val t2 = Transformer.gen[Int, String]
 }
