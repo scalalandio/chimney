42f2d30cb382947ad5b24a0cdc5c45bcfac6b271
Piotr Krzemiński <pio.krzeminski@gmail.com>
2018-04-19 22:45:39 +0200

    uncomment some tests from other suites that pass now

--- chimney/src/test/scala/io/scalaland/chimney/PBTransformationSpec.scala
+++ chimney/src/test/scala/io/scalaland/chimney/PBTransformationSpec.scala
@@ -1,29 +1,29 @@
-//package io.scalaland.chimney
-//
-//import org.scalatest.{MustMatchers, WordSpec}
-//import io.scalaland.chimney.examples.addressbook
-//import io.scalaland.chimney.examples.pb
-//import shapeless.test._
-//
-//class PBTransformationSpec extends WordSpec with MustMatchers {
-//
-//  import dsl._
-//
-//  "Domain to Protobuf" should {
-//
-//    "transform value classes between their primitive representations" in {
-//
-//      addressbook.PersonName("John").transformInto[String] mustBe "John"
-//      addressbook.PersonId(5).transformInto[Int] mustBe 5
-//      addressbook.Email("john@example.com").transformInto[String] mustBe "john@example.com"
-//    }
-//
-//    "not compile if target type is wrong for value class" in {
-//
-//      illTyped(""" addressbook.PersonName("John").transformInto[Int] """)
-//      illTyped(""" addressbook.PersonId(5).transformInto[String] """)
-//      illTyped(""" addressbook.Email("john@example.com").transformInto[Float] """)
-//    }
+package io.scalaland.chimney
+
+import org.scalatest.{MustMatchers, WordSpec}
+import io.scalaland.chimney.examples.addressbook
+import io.scalaland.chimney.examples.pb
+import shapeless.test._
+
+class PBTransformationSpec extends WordSpec with MustMatchers {
+
+  import dsl._
+
+  "Domain to Protobuf" should {
+
+    "transform value classes between their primitive representations" in {
+
+      addressbook.PersonName("John").transformInto[String] mustBe "John"
+      addressbook.PersonId(5).transformInto[Int] mustBe 5
+      addressbook.Email("john@example.com").transformInto[String] mustBe "john@example.com"
+    }
+
+    "not compile if target type is wrong for value class" in {
+
+      illTyped(""" addressbook.PersonName("John").transformInto[Int] """)
+      illTyped(""" addressbook.PersonId(5).transformInto[String] """)
+      illTyped(""" addressbook.Email("john@example.com").transformInto[Float] """)
+    }
 //
 //    "transform enum represented as sealed trait hierarchy" in {
 //
@@ -122,6 +122,6 @@
 //          )
 //      }
 //    }
-//  }
-//
-//}
+  }
+
+}
