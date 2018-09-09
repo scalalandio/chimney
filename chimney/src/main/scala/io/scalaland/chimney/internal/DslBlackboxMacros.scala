e5b429f78af0c64bbf4cd8d56bdbaa21c15c5526
jakub.mikulski <jakub.mikulski@external.kreditech.com>
2018-09-09 13:24:43 +0200

    reading from bean-like classes: added config flag

--- chimney/src/main/scala/io/scalaland/chimney/internal/DslBlackboxMacros.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/DslBlackboxMacros.scala
@@ -26,6 +26,7 @@ trait DslBlackboxMacros {
 
     val emptyT = typeOf[Empty]
     val disableDefaultsT = typeOf[DisableDefaults[_]].typeConstructor
+    val disableBeansT = typeOf[DisableBeanGetterLookup[_]].typeConstructor
     val fieldConstT = typeOf[FieldConst[_, _]].typeConstructor
     val fieldComputedT = typeOf[FieldComputed[_, _]].typeConstructor
     val fieldRelabelledT = typeOf[FieldRelabelled[_, _, _]].typeConstructor
@@ -35,6 +36,8 @@ trait DslBlackboxMacros {
       config
     } else if (cfgTpe.typeConstructor == disableDefaultsT) {
       captureConfiguration(cfgTpe.typeArgs.head, config.copy(disableDefaultValues = true))
+    } else if (cfgTpe.typeConstructor == disableBeansT) {
+      captureConfiguration(cfgTpe.typeArgs.head, config.copy(disableBeanGetterLookup = true))
     } else if (Set(fieldConstT, fieldComputedT).contains(cfgTpe.typeConstructor)) {
       val List(fieldNameT, rest) = cfgTpe.typeArgs
       val fieldName = fieldNameT.singletonString
