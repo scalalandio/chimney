41236fe94e728e8c3f141ebda305c917be0e80eb
Bartosz Kaczkowski <bartosz.kaczkowski@gmail.com>
2018-09-09 13:57:44 +0200

    Added tests for patching - added tests for patching an object with optional fields - refactored a little bit

--- chimney/src/main/scala/io/scalaland/chimney/internal/PatcherMacros.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/PatcherMacros.scala
@@ -14,6 +14,29 @@ trait PatcherMacros {
     val T = weakTypeOf[T]
     val Patch = weakTypeOf[Patch]
 
+    def transformOptionalValue(fnPatch: c.universe.TermName,
+                               pParam: c.universe.MethodSymbol,
+                               tParam: c.universe.MethodSymbol,
+                               fnObj: c.universe.TermName) = {
+      expandTransformerTree(q"$fnPatch.${pParam.name}", Config())(pParam.returnType, tParam.returnType).left
+        .flatMap { errors =>
+          if (pParam.returnType.typeConstructor =:= optionTpe.typeConstructor) {
+            expandTransformerTree(q"$fnPatch.${pParam.name}.get", Config())(
+              pParam.returnType.typeArgs.head,
+              tParam.returnType
+            ).right
+              .map { innerTransformerTree => q"""
+                        if($fnPatch.${pParam.name}.isDefined) { $innerTransformerTree } else { $fnObj.${pParam.name} }
+                       """
+              }
+              .left
+              .map(errors ++ _)
+          } else {
+            Left(DerivationError.printErrors(errors))
+          }
+        }
+    }
+
     if (!T.isCaseClass || !Patch.isCaseClass) {
       c.abort(c.enclosingPosition, s"Patcher derivation is only supported for case classes!")
     } else {
@@ -31,24 +54,7 @@ trait PatcherMacros {
           case Some(tParam) if pParam.returnType <:< tParam.returnType =>
             Right(q"$fnPatch.${pParam.name}")
           case Some(tParam) =>
-            expandTransformerTree(q"$fnPatch.${pParam.name}", Config())(pParam.returnType, tParam.returnType).left
-              .flatMap { errors =>
-                if (pParam.returnType.typeConstructor =:= optionTpe.typeConstructor) {
-                  expandTransformerTree(q"$fnPatch.${pParam.name}.get", Config())(
-                    pParam.returnType.typeArgs.head,
-                    tParam.returnType
-                  ).right
-                    .map { innerTransformerTree =>
-                      q"""
-                        if($fnPatch.${pParam.name}.isDefined) { $innerTransformerTree } else { $fnObj.${pParam.name} }
-                       """
-                    }
-                    .left
-                    .map(errors ++ _)
-                } else {
-                  Left(DerivationError.printErrors(errors))
-                }
-              }
+            transformOptionalValue(fnPatch, pParam, tParam, fnObj)
           case None =>
             Left(s"Field named '${pParam.name}' not found in target patching type $T!")
         }
@@ -65,7 +71,7 @@ trait PatcherMacros {
           patchMapping.getOrElse(tParam.name, q"$fnObj.${tParam.name}")
         }
 
-        val tree = q"""
+        val resultTree = q"""
            new _root_.io.scalaland.chimney.Patcher[$T, $Patch] {
              def patch($fnObj: $T, $fnPatch: $Patch): $T = {
                new $T(..$args)
@@ -73,7 +79,7 @@ trait PatcherMacros {
            }
         """
 
-        c.Expr[io.scalaland.chimney.Patcher[T, Patch]](tree)
+        c.Expr[io.scalaland.chimney.Patcher[T, Patch]](resultTree)
       }
     }
   }
