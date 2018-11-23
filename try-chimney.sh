d2a18f886dd7cdae9a77f7f4a80791e33aab6282
Piotr Krzemiński <pio.krzeminski@gmail.com>
2018-11-23 18:24:04 +0100

    bump chimney version to 0.3.0 in try_chimney.sh

--- try-chimney.sh
+++ try-chimney.sh
@@ -4,5 +4,5 @@ test -e ~/.coursier/coursier || \
   (mkdir -p ~/.coursier && curl -L -s --output ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
 ~/.coursier/coursier launch -q -P -M ammonite.Main \
   com.lihaoyi:ammonite_2.12.7:1.4.4 \
-  io.scalaland:chimney_2.12:0.2.1 \
+  io.scalaland:chimney_2.12:0.3.0 \
   -- --predef-code 'import io.scalaland.chimney.dsl._' < /dev/tty
