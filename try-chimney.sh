f1750a048007feb0667d96d33939dcb8c8ed83be
Piotr Krzemiński <pio.krzeminski@gmail.com>
2019-06-14 22:27:15 +0200

    bump versions in try-chimney.sh

--- try-chimney.sh
+++ try-chimney.sh
@@ -3,6 +3,6 @@ COURSIER_URL=https://git.io/vgvpD
 test -e ~/.coursier/coursier || \
   (mkdir -p ~/.coursier && curl -L -s --output ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
 ~/.coursier/coursier launch -q -P -M ammonite.Main \
-  com.lihaoyi:ammonite_2.12.8:1.6.4 \
-  io.scalaland:chimney_2.12:0.3.1 \
+  com.lihaoyi:ammonite_2.13.0:1.6.8 \
+  io.scalaland:chimney_2.13:0.3.2 \
   -- --predef-code 'import io.scalaland.chimney.dsl._' < /dev/tty
