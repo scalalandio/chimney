88820b1b09a9d2b2a969e3f0efd99e24788865fd
Piotr Krzemiński <pio.krzeminski@gmail.com>
2020-01-18 09:36:26 +0100

    fix macro return type

--- chimney/src/main/scala/io/scalaland/chimney/internal/macros/ChimneyBlackboxMacros.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/macros/ChimneyBlackboxMacros.scala
@@ -2,6 +2,7 @@ package io.scalaland.chimney.internal.macros
 
 import io.scalaland.chimney.internal.DerivationConfig
 import io.scalaland.chimney.internal.utils.{DerivationGuards, EitherUtils, MacroUtils}
+import io.scalaland.chimney.{Patcher, Transformer}
 
 import scala.reflect.macros.blackbox
 
@@ -14,21 +15,20 @@ class ChimneyBlackboxMacros(val c: blackbox.Context)
     with DerivationConfig
     with EitherUtils {
 
-  def buildTransformerImpl[From: c.WeakTypeTag, To: c.WeakTypeTag, C: c.WeakTypeTag]: c.Expr[To] = {
-    c.Expr[To](buildDefinedTransformer[From, To, C])
+  def buildTransformerImpl[From: c.WeakTypeTag, To: c.WeakTypeTag, C: c.WeakTypeTag]: c.Expr[Transformer[From, To]] = {
+    c.Expr[Transformer[From, To]](buildDefinedTransformer[From, To, C])
   }
 
   def transformImpl[From: c.WeakTypeTag, To: c.WeakTypeTag, C: c.WeakTypeTag]: c.Expr[To] = {
     c.Expr[To](expandTransform[From, To, C])
   }
 
-  def deriveTransformerImpl[From: c.WeakTypeTag, To: c.WeakTypeTag]
-      : c.Expr[io.scalaland.chimney.Transformer[From, To]] = {
+  def deriveTransformerImpl[From: c.WeakTypeTag, To: c.WeakTypeTag]: c.Expr[Transformer[From, To]] = {
     import c.universe._
     genTransformer[From, To](Config(definitionScope = Some((weakTypeOf[From], weakTypeOf[To]))))
   }
 
-  def derivePatcherImpl[T: c.WeakTypeTag, Patch: c.WeakTypeTag]: c.Expr[io.scalaland.chimney.Patcher[T, Patch]] = {
+  def derivePatcherImpl[T: c.WeakTypeTag, Patch: c.WeakTypeTag]: c.Expr[Patcher[T, Patch]] = {
     genPatcher[T, Patch]()
   }
 }
