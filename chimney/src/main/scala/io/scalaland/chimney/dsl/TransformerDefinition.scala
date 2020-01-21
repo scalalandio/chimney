9049623d6b401d52158aa3fd64231343343116da
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2020-01-21 15:03:37 +0100

    Initial doumentation for public interfaces related to Transformer

--- chimney/src/main/scala/io/scalaland/chimney/dsl/TransformerDefinition.scala
+++ chimney/src/main/scala/io/scalaland/chimney/dsl/TransformerDefinition.scala
@@ -7,37 +7,122 @@ import io.scalaland.chimney.internal.macros.{ChimneyBlackboxMacros, TransformerD
 
 import scala.language.experimental.macros
 
+/** Allows customization of [[io.scalaland.chimney.Transformer]] derivation
+  * 
+  * By default derivation will expect that:
+  * 
+  * - for every field in `To` there is a corresponding field in `From`
+  * - their types match or it is possible to find/derive [[io.scalaland.chimney.Transformer]] for them
+  * 
+  * For some case this is insufficient and a manual intervention is needed:
+  * 
+  * - derivation should be informed that field in `From` corresponds with field
+  *   of a different name in `To`,
+  * - derivation should be informed that missing `To` field's value should be
+  *   a constant/computed value,
+  * - convertion to/from `Option` should get a special treatment,
+  * - input/output type is a Java Bean with a convention different to Scala one's.
+  * 
+  * In such case you can use methods of this class to change derivation configuration.
+  * 
+  * {{{
+  * implicit val fooToBar: Transformer[Foo, Bar] = Transformer.define[Foo, Bar]
+  *   .setting1
+  *   .setting2
+  *   .buildTransformer
+  * }}}
+  * 
+  * @see [[io.scalaland.chimney.dsl.TransformerInto]] for building transformer and applying it immediately
+  * 
+  * @tparam From data type that will be used as input
+  * @tparam To   data type that will be used as output
+  * @tparam C    type-level encoded list of settings
+  *              (implementation detail, but it is important to not change it manually)
+  */
 final class TransformerDefinition[From, To, C <: TransformerCfg](
     val overrides: Map[String, Any],
     val instances: Map[(String, String), Any]
 ) {
+
+  /** Fail derivation if `From` type is missing field even if `To` has default value for it
+    * 
+    * By default in such case derivation will fallback to default values.
+    */
   def disableDefaultValues: TransformerDefinition[From, To, DisableDefaultValues[C]] =
     this.asInstanceOf[TransformerDefinition[From, To, DisableDefaultValues[C]]]
 
+  /** Enable Java Beans naming convention (`.getName`, `.isName`) on `From`
+    * 
+    * By default only Scala conversions (`.name`) are allowed.
+    */
   def enableBeanGetters: TransformerDefinition[From, To, EnableBeanGetters[C]] =
     this.asInstanceOf[TransformerDefinition[From, To, EnableBeanGetters[C]]]
 
+  /** Enable Java Beans naming convention (`.setName`) on `To`
+    * 
+    * By default only Scala conversions (`.name =`) are allowed.
+    */
   def enableBeanSetters: TransformerDefinition[From, To, EnableBeanSetters[C]] =
     this.asInstanceOf[TransformerDefinition[From, To, EnableBeanSetters[C]]]
 
+  /** Allow usage of None if field is missing in `From`, no rename/const value/calulated
+    * value is available but present in `To` as `Option` type
+    * 
+    * By default in such case derivation will fail.
+    */
   def enableOptionDefaultsToNone: TransformerDefinition[From, To, EnableOptionDefaultsToNone[C]] =
     this.asInstanceOf[TransformerDefinition[From, To, EnableOptionDefaultsToNone[C]]]
 
+  /** Allow running `.get` if `From` contains `Option[A]` and `To` requires `A`
+    * 
+    * By default in such case derivation will fail.
+    */
   def enableUnsafeOption: TransformerDefinition[From, To, EnableUnsafeOption[C]] =
     this.asInstanceOf[TransformerDefinition[From, To, EnableUnsafeOption[C]]]
 
+  /** Use `value` provided here for field picked using `selector`.
+    * 
+    * By default if `From` is missing field picked by `selector` derivation will fail.
+    * 
+    * @param selector target field in `To`, defined like `_.name`
+    * @param value    constant value to use for the target field
+    */
   def withFieldConst[T, U](selector: To => T, value: U): TransformerDefinition[From, To, _] =
     macro TransformerDefinitionWhiteboxMacros.withFieldConstImpl[From, To, T, U, C]
 
+  /** Use `map` provided here to compute value of field picked using `selector`.
+    * 
+    * By default if `From` is missing field picked by `selector` derivation will fail.
+    * 
+    * @param selector target field in `To`, defined like `_.name`
+    * @param map      function to use to compute value of the target field
+    */
   def withFieldComputed[T, U](selector: To => T, map: From => U): TransformerDefinition[From, To, _] =
     macro TransformerDefinitionWhiteboxMacros.withFieldComputedImpl[From, To, T, U, C]
 
+  /** Use `selectorFrom` field in `From` to obtain the value of `selectorTo` field in `To`
+    * 
+    * By default if `From` is missing field picked by `selectorTo` derivation will fail.
+    *
+    * @param selectorFrom source field in `From`, defined like `_.originalName`
+    * @param selectorTo   target field in `To`, defined like `_.newName`
+    */
   def withFieldRenamed[T, U](selectorFrom: From => T, selectorTo: To => U): TransformerDefinition[From, To, _] =
     macro TransformerDefinitionWhiteboxMacros.withFieldRenamedImpl[From, To, T, U, C]
 
+  /** Use `f` to calculate the (missing) coproduct instance when mapping one coproduct into another
+    * 
+    * By default if mapping one coproduct in `From` into another coproduct in `To` derivation
+    * expects that coproducts will have matching names of its components, and for every component
+    * in `To` field's type there is matching component in `From` type. If some component is missing
+    * it will fail.
+    *
+    * @param f function to calculate values of components that cannot be mapped automatically
+    */
   def withCoproductInstance[Inst](f: Inst => To): TransformerDefinition[From, To, _] =
     macro TransformerDefinitionWhiteboxMacros.withCoproductInstanceImpl[From, To, Inst, C]
 
+  /** Build Transformer using current configuration */
   def buildTransformer: Transformer[From, To] =
     macro ChimneyBlackboxMacros.buildTransformerImpl[From, To, C]
 }
--- chimney/src/main/scala/io/scalaland/chimney/dsl/TransformerInto.scala
+++ chimney/src/main/scala/io/scalaland/chimney/dsl/TransformerInto.scala
@@ -6,38 +6,135 @@ import io.scalaland.chimney.internal.macros.{ChimneyBlackboxMacros, TransformerI
 
 import scala.language.experimental.macros
 
+/** Provides DSL for configuring [[io.scalaland.chimney.Transformer]]'s
+  * generation and using the result to transform value at the same time
+  * 
+  * For any ''.setting'' defined here (and in [[io.scalaland.chimney.dsl.TransformerDefinition]])
+  * we can:
+  * 
+  * {{{
+  * import io.scalaland.chimney.dsl._
+  * foo.into[Bar] // <- creates this builder initializing it with foo as source and empty config
+  *   .setting    // <- calls .setting on TransformerDefinition
+  *   .transform  // <- calls .buildTransformer.transform(foo)
+  * }}}
+  * 
+  * or (if only defaults are used, or there is some implicit [[io.scalaland.chimney.Transformer]]
+  * already in scope):
+  * 
+  * {{{
+  * import io.scalaland.chimney.dsl._
+  * foo.transformInto[Bar]
+  * }}}
+  * 
+  * @see [[io.scalaland.chimney.dsl.TransformerDefinition]] for building Transformer separately to applying it (in case you want to share it between several transformations)
+  * 
+  * @tparam From data type that will be used as input
+  * @tparam To   data type that will be used as output
+  * @tparam C    type-level encoded list of settings
+  */
 final class TransformerInto[From, To, C <: TransformerCfg](
     val source: From,
     val td: TransformerDefinition[From, To, C]
 ) {
 
+  /** Fail derivation if `From` type is missing field even if `To` has default value for it
+    * 
+    * By default in such case derivation will fallback to default values.
+    * 
+    * @see [[io.scalaland.chimney.dsl.TransformerDefinition#disableDefaultValues]]
+    */
   def disableDefaultValues: TransformerInto[From, To, DisableDefaultValues[C]] =
     this.asInstanceOf[TransformerInto[From, To, DisableDefaultValues[C]]]
 
+  /** Enable Java Beans naming convention (`.getName`, `.isName`) on `From`
+    * 
+    * By default only Scala conversions (`.name`) are allowed.
+    * @see [[io.scalaland.chimney.dsl.TransformerDefinition#enableBeanGetters]]
+    */
   def enableBeanGetters: TransformerInto[From, To, EnableBeanGetters[C]] =
     this.asInstanceOf[TransformerInto[From, To, EnableBeanGetters[C]]]
 
+  /** Enable Java Beans naming convention (`.setName`) on `To`
+    * 
+    * By default only Scala conversions (`.name =`) are allowed.
+    * 
+    * @see [[io.scalaland.chimney.dsl.TransformerDefinition#enableBeanSetters]]
+    */
   def enableBeanSetters: TransformerInto[From, To, EnableBeanSetters[C]] =
     this.asInstanceOf[TransformerInto[From, To, EnableBeanSetters[C]]]
 
+  /** Allow usage of None if field is missing in `From`, no rename/const value/calulated
+    * value is available but present in `To` as `Option` type
+    * 
+    * By default in such case derivation will fail.
+    * 
+    * @see [[io.scalaland.chimney.dsl.TransformerDefinition#enableOptionDefaultsToNone]]
+    */
   def enableOptionDefaultsToNone: TransformerInto[From, To, EnableOptionDefaultsToNone[C]] =
     this.asInstanceOf[TransformerInto[From, To, EnableOptionDefaultsToNone[C]]]
 
+  /** Allow running `.get` if `From` contains `Option[A]` and `To` requires `A`
+    * 
+    * By default in such case derivation will fail.
+    * 
+    * @see [[io.scalaland.chimney.dsl.TransformerDefinition#enableUnsafeOption]]
+    * */
   def enableUnsafeOption: TransformerInto[From, To, EnableUnsafeOption[C]] =
     this.asInstanceOf[TransformerInto[From, To, EnableUnsafeOption[C]]]
 
+  /** Use `value` provided here for field picked using `selector`.
+    * 
+    * By default if `From` is missing field picked by `selector` derivation will fail.
+    * 
+    * @see [[io.scalaland.chimney.dsl.TransformerDefinition#withFieldConst]] 
+    * 
+    * @param selector target field in `To`, defined like `_.name`
+    * @param value    constant value to use for the target field
+    */
   def withFieldConst[T, U](selector: To => T, value: U): TransformerInto[From, To, _] =
     macro TransformerIntoWhiteboxMacros.withFieldConstImpl[From, To, T, U, C]
 
+  /** Use `map` provided here to compute value of field picked using `selector`.
+    * 
+    * By default if `From` is missing field picked by `selector` derivation will fail.
+    * 
+    * @see [[io.scalaland.chimney.dsl.TransformerDefinition#withFieldComputed]] 
+    * 
+    * @param selector target field in `To`, defined like `_.name`
+    * @param map      function to use to compute value of the target field
+    * */
   def withFieldComputed[T, U](selector: To => T, map: From => U): TransformerInto[From, To, _] =
     macro TransformerIntoWhiteboxMacros.withFieldComputedImpl[From, To, T, U, C]
 
+  /** Use `selectorFrom` field in `From` to obtain the value of `selectorTo` field in `To`
+    * 
+    * By default if `From` is missing field picked by `selectorTo` derivation will fail.
+    *
+    * @see [[io.scalaland.chimney.dsl.TransformerDefinition#withFieldRenamed]] 
+    * 
+    * @param selectorFrom source field in `From`, defined like `_.originalName`
+    * @param selectorTo   target field in `To`, defined like `_.newName`
+    * */
   def withFieldRenamed[T, U](selectorFrom: From => T, selectorTo: To => U): TransformerInto[From, To, _] =
     macro TransformerIntoWhiteboxMacros.withFieldRenamedImpl[From, To, T, U, C]
 
+  /** Use `f` to calculate the (missing) coproduct instance when mapping one coproduct into another
+    * 
+    * By default if mapping one coproduct in `From` into another coproduct in `To` derivation
+    * expects that coproducts will have matching names of its components, and for every component
+    * in `To` field's type there is matching component in `From` type. If some component is missing
+    * it will fail.
+    * 
+    * @see [[io.scalaland.chimney.dsl.TransformerDefinition#withCoproductInstance]]
+    *
+    * @param f function to calculate values of components that cannot be mapped automatically
+    * 
+    * @see [[io.scalaland.chimney.dsl.TransformerDefinition#withCoproductInstance]]*/
   def withCoproductInstance[Inst](f: Inst => To): TransformerInto[From, To, _] =
     macro TransformerIntoWhiteboxMacros.withCoproductInstanceImpl[From, To, Inst, C]
 
+  /** Derive [[io.scalaland.chimney.Transformer]] and pass transformed value into it */
   def transform: To =
     macro ChimneyBlackboxMacros.transformImpl[From, To, C]
 }
