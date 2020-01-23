809696cc7244bd3a423fd040752cbe045e6c7ccb
Piotr Krzemiński <pio.krzeminski@gmail.com>
2020-01-23 14:09:52 +0100

    scala code formatting

--- chimney/src/main/scala/io/scalaland/chimney/Patcher.scala
+++ chimney/src/main/scala/io/scalaland/chimney/Patcher.scala
@@ -5,10 +5,10 @@ import io.scalaland.chimney.internal.macros.ChimneyBlackboxMacros
 import scala.language.experimental.macros
 
 /** Type class definition that wraps patching behavior.
- *
- * @tparam T type of object to apply patch to
- * @tparam Patch type of patch object
- */
+  *
+  * @tparam T type of object to apply patch to
+  * @tparam Patch type of patch object
+  */
 trait Patcher[T, Patch] {
   def patch(obj: T, patch: Patch): T
 }
@@ -16,12 +16,12 @@ trait Patcher[T, Patch] {
 object Patcher {
 
   /** Provides implicit [[io.scalaland.chimney.Patcher]] instance
-   * for arbitrary types.
-   *
-   * @tparam T type of object to apply patch to
-   * @tparam Patch type of patch object
-   * @return [[io.scalaland.chimney.Patcher]] type class instance
-   */
+    * for arbitrary types.
+    *
+    * @tparam T type of object to apply patch to
+    * @tparam Patch type of patch object
+    * @return [[io.scalaland.chimney.Patcher]] type class instance
+    */
   implicit def derive[T, Patch]: Patcher[T, Patch] =
     macro ChimneyBlackboxMacros.derivePatcherImpl[T, Patch]
 }
