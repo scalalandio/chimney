9f6fc5e6ab78c7f80a51f828552f4cfea02820d5
Piotr Krzemiński <pio.krzeminski@gmail.com>
2020-02-03 11:44:24 +0700

    ammonite 2.0.4 and chimney 0.4.1 in try-chimney.sh

--- try-chimney.sh
+++ try-chimney.sh
@@ -3,6 +3,6 @@ COURSIER_URL=https://git.io/vgvpD
 test -e ~/.coursier/coursier || \
   (mkdir -p ~/.coursier && curl -L -s --output ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
 ~/.coursier/coursier launch -q -P -M ammonite.Main \
-  com.lihaoyi:ammonite_2.13.1:1.8.1 \
-  io.scalaland:chimney_2.13:0.3.4 \
+  com.lihaoyi:ammonite_2.13.1:2.0.4 \
+  io.scalaland:chimney_2.13:0.4.1 \
   -- --predef-code 'import io.scalaland.chimney.dsl._' < /dev/tty
