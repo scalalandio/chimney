80c6ace8dd005b08e45f5d5032b00ea4a92d4f9d
Piotr Krzemiński <pio.krzeminski@gmail.com>
2020-03-17 18:52:53 +0100

    bump version and enable chimney-cats in try-chimney.sh script

--- try-chimney.sh
+++ try-chimney.sh
@@ -4,5 +4,7 @@ test -e ~/.coursier/coursier || \
   (mkdir -p ~/.coursier && curl -L -s --output ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
 ~/.coursier/coursier launch -q -P -M ammonite.Main \
   com.lihaoyi:ammonite_2.13.1:2.0.4 \
-  io.scalaland:chimney_2.13:0.4.1 \
-  -- --predef-code 'import io.scalaland.chimney.dsl._' < /dev/tty
+  org.typelevel:cats-core_2.13:2.1.1 \
+  io.scalaland:chimney_2.13:0.5.0 \
+  io.scalaland:chimney-cats_2.13:0.5.0 \
+  -- --predef-code 'import io.scalaland.chimney.dsl._;import io.scalaland.chimney.cats._' < /dev/tty
