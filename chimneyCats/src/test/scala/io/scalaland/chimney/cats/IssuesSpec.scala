4669be938ec3c8ff6c43218169b404253a24790e
Mark <mark@vectos.net>
2022-02-17 09:46:23 +0100

    Reproduced the issue

--- chimneyCats/src/test/scala/io/scalaland/chimney/cats/IssuesSpec.scala
+++ chimneyCats/src/test/scala/io/scalaland/chimney/cats/IssuesSpec.scala
@@ -7,26 +7,24 @@ import utest._
 object IssuesSpec extends TestSuite {
   val tests: Tests = Tests {
     "fix issue #214" - {
-      final case class BillingRowRaw(
+      final case class Foo(
                                       `Billing Zip/Postal Code`: String,
                                       `Shipping Zip/Postal Code`: String,
                                       `Billing Supplier Country (text only)`: String
                                     )
 
-      final case class BillingRow(
-                                   billingPostalCode: String,
-                                   shippingPostalCode: String,
-                                   billingCountry: String
+      final case class Bar(
+                            `Billing Zip/Postal Code`: String,
+                            `Shipping Zip/Postal Code`: String,
+                            `Billing Supplier Country (text only)`: String
                                  )
 
-      val transformer = Transformer.defineF[ValidatedNec[String, +*], BillingRowRaw, BillingRow]
-        .withFieldRenamed(_.`Billing Zip/Postal Code`, _.billingPostalCode)
-        .withFieldRenamed(_.`Shipping Zip/Postal Code`, _.shippingPostalCode)
-        .withFieldRenamed(_.`Billing Supplier Country (text only)`, _.billingCountry)
+      val transformer = Transformer
+        .defineF[ValidatedNec[String, +*], Foo, Bar]
         .buildTransformer
 
-      val expected = BillingRow("3152XX", "3152XX", "England")
-      val result = transformer.transform(BillingRowRaw("3152XX", "3152XX", "England"))
+      val expected = Bar("3152XX", "3152XX", "England")
+      val result = transformer.transform(Foo("3152XX", "3152XX", "England"))
       assert(result == Validated.validNec(expected))
     }
   }
