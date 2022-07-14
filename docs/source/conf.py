7c9e8da09009813ee1ea788ed81a4c56fa785b24
Piotr Krzemiński <pio.krzeminski@gmail.com>
2022-07-14 18:33:51 +0200

    update docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Chimney'
-copyright = u'2017-2020, scalaland.io'
+copyright = u'2017-2022, scalaland.io'
 author = u'Piotr Krzemiński, Mateusz Kubuszok'
 
 # The version info for the project you're documenting, acts as replacement for
