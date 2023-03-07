66515ddf00bd98b2b0b320a6c8bb8081ef32bd9b
Piotr Krzemiński <pio.krzeminski@gmail.com>
2023-03-07 20:33:10 +0100

    bump version to 0.7.1

--- try-chimney.sh
+++ try-chimney.sh
@@ -6,6 +6,6 @@ test -e ~/.coursier/coursier || (mkdir -p ~/.coursier && curl -fLo ~/.coursier/c
 ~/.coursier/coursier launch -q -P -M ammonite.Main \
   com.lihaoyi:ammonite_2.13.3:2.5.8 \
   org.typelevel:cats-core_2.13:2.9.0 \
-  io.scalaland:chimney_2.13:0.7.0 \
-  io.scalaland:chimney-cats_2.13:0.7.0 \
+  io.scalaland:chimney_2.13:0.7.1 \
+  io.scalaland:chimney-cats_2.13:0.7.1 \
   -- --predef-code 'import $plugin.$ivy.`org.typelevel:kind-projector_2.13.10:0.13.2`;import io.scalaland.chimney.dsl._;import io.scalaland.chimney.cats._' < /dev/tty
