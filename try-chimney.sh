166e64344cbe273a7700f03c361decef705a17d1
Piotr Krzemiński <pio.krzeminski@gmail.com>
2023-03-07 20:32:29 +0100

    fix try-chimney.sh to work with latest coursier/ammonite

--- try-chimney.sh
+++ try-chimney.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-COURSIER_URL=https://git.io/coursier-cli
-test -e ~/.coursier/coursier || \
-  (mkdir -p ~/.coursier && curl -L -s --output ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
+COURSIER_URL=https://github.com/coursier/launchers/raw/master/coursier
+
+test -e ~/.coursier/coursier || (mkdir -p ~/.coursier && curl -fLo ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
+
 ~/.coursier/coursier launch -q -P -M ammonite.Main \
-  com.lihaoyi:ammonite_2.13.3:2.2.0 \
-  org.typelevel:cats-core_2.13:2.2.0 \
+  com.lihaoyi:ammonite_2.13.3:2.5.8 \
+  org.typelevel:cats-core_2.13:2.9.0 \
   io.scalaland:chimney_2.13:0.7.0 \
   io.scalaland:chimney-cats_2.13:0.7.0 \
-  -- --predef-code 'import $plugin.$ivy.`org.typelevel:kind-projector_2.13.1:0.11.0`;import io.scalaland.chimney.dsl._;import io.scalaland.chimney.cats._' < /dev/tty
+  -- --predef-code 'import $plugin.$ivy.`org.typelevel:kind-projector_2.13.10:0.13.2`;import io.scalaland.chimney.dsl._;import io.scalaland.chimney.cats._' < /dev/tty
