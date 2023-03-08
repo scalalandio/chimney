66fd2fc776a96cf86d77231ceec61f9dc389940e
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2023-03-08 10:54:21 +0100

    Remove lifted transformers from benchmarks

--- benchmarks/src/main/scala/io/scalaland/chimney/benchmarks/ErrorAccSimple.scala
+++ benchmarks/src/main/scala/io/scalaland/chimney/benchmarks/ErrorAccSimple.scala
@@ -8,25 +8,9 @@ class ErrorAccSimple extends CommonBenchmarkSettings {
   import fixtures.*
   import samples.validation.*
 
-  type M[+A] = Either[Vector[TransformationError[String]], A]
-
-  @Benchmark
-  def simpleHappyChimneyDefinedLifted: M[SimpleOutput] =
-    transformers.simpleTransformerLiftedHappy.transform(samples.simpleSample)
-
   @Benchmark
   def simpleHappyChimneyDefinedPartial: partial.Result[SimpleOutput] =
     transformers.simpleTransformerPartialHappy.transform(samples.simpleSample)
-
-  @Benchmark
-  def simpleHappyChimneyIntoLifted: M[SimpleOutput] =
-    samples.simpleSample
-      .intoF[M, SimpleOutput]
-      .withFieldComputedF(_.a, s => happy.validateA(s.a).left.map(s => Vector(TransformationError(s))))
-      .withFieldComputedF(_.b, s => happy.validateB(s.b).left.map(s => Vector(TransformationError(s))))
-      .withFieldComputedF(_.c, s => happy.validateC(s.c).left.map(s => Vector(TransformationError(s))))
-      .withFieldComputedF(_.d, s => happy.validateD(s.d).left.map(s => Vector(TransformationError(s))))
-      .transform
 
   @Benchmark
   def simpleHappyChimneyIntoPartial: partial.Result[SimpleOutput] =
@@ -39,42 +23,8 @@ class ErrorAccSimple extends CommonBenchmarkSettings {
       .transform
 
   @Benchmark
-  def simpleHappyByHandEitherSwap: M[SimpleOutput] =
-    simpleByHandErrorAccEitherSwap(
-      samples.simpleSample,
-      happy.validateA(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("a")))),
-      happy.validateB(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("b")))),
-      happy.validateC(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("c")))),
-      happy.validateD(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("d"))))
-    )
-
-  @Benchmark
-  def simpleHappyByHandCrazyNesting: M[SimpleOutput] =
-    simpleByHandErrorAccCrazyNesting(
-      samples.simpleSample,
-      happy.validateA(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("a")))),
-      happy.validateB(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("b")))),
-      happy.validateC(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("c")))),
-      happy.validateD(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("d"))))
-    )
-
-  @Benchmark
-  def simpleUnhappyChimneyDefinedLifted: M[SimpleOutput] =
-    transformers.simpleTransformerLiftedUnhappy.transform(samples.simpleSample)
-
-  @Benchmark
   def simpleUnhappyChimneyDefinedPartial: partial.Result[SimpleOutput] =
     transformers.simpleTransformerPartialUnhappy.transform(samples.simpleSample)
-
-  @Benchmark
-  def simpleUnhappyChimneyIntoLifted: M[SimpleOutput] =
-    samples.simpleSample
-      .intoF[M, SimpleOutput]
-      .withFieldComputedF(_.a, s => unhappy.validateA(s.a).left.map(s => Vector(TransformationError(s))))
-      .withFieldComputedF(_.b, s => unhappy.validateB(s.b).left.map(s => Vector(TransformationError(s))))
-      .withFieldComputedF(_.c, s => unhappy.validateC(s.c).left.map(s => Vector(TransformationError(s))))
-      .withFieldComputedF(_.d, s => unhappy.validateD(s.d).left.map(s => Vector(TransformationError(s))))
-      .transform
 
   @Benchmark
   def simpleUnhappyChimneyIntoPartial: partial.Result[SimpleOutput] =
@@ -85,24 +35,4 @@ class ErrorAccSimple extends CommonBenchmarkSettings {
       .withFieldComputedPartial(_.c, s => unhappy.validateC(s.c).toPartialResult)
       .withFieldComputedPartial(_.d, s => unhappy.validateD(s.d).toPartialResult)
       .transform
-
-  @Benchmark
-  def simpleUnhappyByHandEitherSwap: M[SimpleOutput] =
-    simpleByHandErrorAccEitherSwap(
-      samples.simpleSample,
-      unhappy.validateA(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("a")))),
-      unhappy.validateB(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("b")))),
-      unhappy.validateC(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("c")))),
-      unhappy.validateD(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("d"))))
-    )
-
-  @Benchmark
-  def simpleUnhappyByHandCrazyNesting: M[SimpleOutput] =
-    simpleByHandErrorAccCrazyNesting(
-      samples.simpleSample,
-      unhappy.validateA(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("a")))),
-      unhappy.validateB(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("b")))),
-      unhappy.validateC(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("c")))),
-      unhappy.validateD(_).left.map(s => Vector(TransformationError(s).prepend(ErrorPathNode.Accessor("d"))))
-    )
 }
