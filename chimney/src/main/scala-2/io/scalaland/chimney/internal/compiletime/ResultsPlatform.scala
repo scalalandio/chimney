20363f8a105b9272c3622cad1116fc5791fc32cb
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2023-04-18 23:15:45 +0100

    Remove resolved todo in reportError

--- chimney/src/main/scala-2/io/scalaland/chimney/internal/compiletime/ResultsPlatform.scala
+++ chimney/src/main/scala-2/io/scalaland/chimney/internal/compiletime/ResultsPlatform.scala
@@ -1,7 +1,6 @@
 package io.scalaland.chimney.internal.compiletime
 
 private[compiletime] trait ResultsPlatform extends Results { this: DefinitionsPlatform =>
-
-  // TODO: proper error generation
+  
   protected def reportError(errors: String): Nothing = c.abort(c.enclosingPosition, errors)
 }
