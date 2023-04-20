b73101cdcc32b2b983cefaf90234a8eca6e4c208
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2023-04-20 18:47:00 +0100

    Move Rule to Derivation since it's the only place which would make use if it

--- chimney/src/main/scala/io/scalaland/chimney/internal/compiletime/derivation/transformer/Derivation.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/compiletime/derivation/transformer/Derivation.scala
@@ -8,6 +8,31 @@ import scala.annotation.nowarn
 @nowarn("msg=The outer reference in this type test cannot be checked at run time.")
 private[derivation] trait Derivation { this: Definitions & Legacy =>
 
+  /** Intended use case: recursive derivation */
+  final protected def deriveTransformationResultExpr[From, To](implicit
+      ctx: TransformerContext[From, To]
+  ): DerivationResult[DerivedExpr[To]] = Rule[From, To]
+
+  abstract protected class Rule {
+
+    def isApplicableTo[From, To](ctx: TransformerContext[From, To]): Boolean
+
+    def apply[From, To](implicit ctx: TransformerContext[From, To]): DerivationResult[DerivedExpr[To]]
+  }
+  protected object Rule {
+
+    def apply[From, To](
+        rules: Seq[Rule]
+    )(implicit ctx: TransformerContext[From, To]): DerivationResult[DerivedExpr[To]] =
+      rules.find(_.isApplicableTo(ctx)) match {
+        case Some(rule) => rule[From, To]
+        case None       => DerivationResult.notSupportedTransformerDerivation
+      }
+
+    def apply[From, To](implicit ctx: TransformerContext[From, To]): DerivationResult[DerivedExpr[To]] =
+      apply(rulesAvailableForPlatform)
+  }
+
   protected def instantiateTotalTransformer[From: Type, To: Type](
       f: Expr[From] => DerivationResult[Expr[To]]
   ): DerivationResult[Expr[Transformer[From, To]]]
@@ -16,9 +41,5 @@ private[derivation] trait Derivation { this: Definitions & Legacy =>
       f: (Expr[From], Expr[Boolean]) => DerivationResult[Expr[partial.Result[To]]]
   ): DerivationResult[Expr[PartialTransformer[From, To]]]
 
-  /** Intended use case: recursive derivation */
-  final protected def deriveTransformationResultExpr[From, To](implicit
-      ctx: TransformerContext[From, To]
-  ): DerivationResult[DerivedExpr[To]] =
-    DerivationResult.notYetImplemented("Actual derivation")
+  protected val rulesAvailableForPlatform: Seq[Rule]
 }
