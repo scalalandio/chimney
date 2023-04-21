cec1b36acf616cf5d341251feada140aaad53f3e
Piotr Krzemiński <pio.krzeminski@gmail.com>
2023-04-21 16:45:18 +0200

    scala 3 flags parsing

--- chimney/src/main/scala-3/io/scalaland/chimney/internal/compiletime/ConfigurationsPlatform.scala
+++ chimney/src/main/scala-3/io/scalaland/chimney/internal/compiletime/ConfigurationsPlatform.scala
@@ -5,6 +5,9 @@ import io.scalaland.chimney.internal
 
 private[compiletime] trait ConfigurationsPlatform extends Configurations { this: DefinitionsPlatform =>
 
+  import quotes.*
+  import quotes.reflect.*
+
   protected object configurationsImpl extends ConfigurationDefinitionsImpl {
 
     final override def extractRuntimeConfiguration[From: Type, ToField: Type](
@@ -26,7 +29,32 @@ private[compiletime] trait ConfigurationsPlatform extends Configurations { this:
 
     private def extractTransformerFlags[Flag <: internal.TransformerFlags: Type](
         defaultFlags: TransformerFlags
-    ): TransformerFlags = ???
+    ): TransformerFlags = {
+      val flags = TypeRepr.of[Flag].dealias
+
+      flags.asType match {
+        case '[internal.TransformerFlags.Default] =>
+          defaultFlags
+        case '[internal.TransformerFlags.Enable[flag, flags]] =>
+          val flagsRest = extractTransformerFlags[flags](defaultFlags)
+          Type[flag] match
+            case '[internal.TransformerFlags.ImplicitConflictResolution[dsls.PreferTotalTransformer.type]] =>
+              flagsRest.setImplicitConflictResolution(Some(dsls.PreferTotalTransformer))
+            case '[internal.TransformerFlags.ImplicitConflictResolution[dsls.PreferPartialTransformer.type]] =>
+              flagsRest.setImplicitConflictResolution(Some(dsls.PreferPartialTransformer))
+            case _ =>
+              flagsRest.setBoolFlag[flag](value = true)
+        case '[internal.TransformerFlags.Disable[flag, flags]] =>
+          val flagsRest = extractTransformerFlags[flags](defaultFlags)
+          Type[flag] match
+            case '[internal.TransformerFlags.ImplicitConflictResolution[?]] =>
+              flagsRest.setImplicitConflictResolution(None)
+            case _ =>
+              flagsRest.setBoolFlag[flag](value = false)
+        case _ =>
+          reportError("Bad internal transformer flags type shape!")
+      }
+    }
 
     private def extractTransformerConfig[From: Type, To: Type, Cfg <: internal.TransformerCfg: Type](
         runtimeDataIdx: Int
