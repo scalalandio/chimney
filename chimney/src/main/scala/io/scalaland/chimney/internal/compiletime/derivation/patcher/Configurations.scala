ff01c44f87fec650c79fce6ae16343b8aebae6ad
Piotr Krzemiński <pio.krzeminski@gmail.com>
2023-07-04 20:39:25 +0200

    code formatting

--- chimney/src/main/scala/io/scalaland/chimney/internal/compiletime/derivation/patcher/Configurations.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/compiletime/derivation/patcher/Configurations.scala
@@ -16,27 +16,25 @@ private[compiletime] trait Configurations { this: Derivation =>
       readPatcherConfigAux(PatcherConfig())
     }
 
-    private def readPatcherConfigAux[Cfg <: internal.PatcherCfg: Type](cfg: PatcherConfig): PatcherConfig = Type[Cfg] match    {
-      case empty if empty =:= ChimneyType.PatcherCfg.Empty => cfg
-      case ChimneyType.PatcherCfg.IgnoreRedundantPatcherFields(cfgRest) =>
-        ExistentialType.Bounded.use(cfgRest) {
-          implicit CfgRest: Type[cfgRest.Underlying] =>
+    private def readPatcherConfigAux[Cfg <: internal.PatcherCfg: Type](cfg: PatcherConfig): PatcherConfig =
+      Type[Cfg] match {
+        case empty if empty =:= ChimneyType.PatcherCfg.Empty => cfg
+        case ChimneyType.PatcherCfg.IgnoreRedundantPatcherFields(cfgRest) =>
+          ExistentialType.Bounded.use(cfgRest) { implicit CfgRest: Type[cfgRest.Underlying] =>
             readPatcherConfigAux[cfgRest.Underlying](cfg).copy(ignoreRedundantPatcherFields = true)
-        }
-      case ChimneyType.PatcherCfg.IgnoreNoneInPatch(cfgRest) =>
-        ExistentialType.Bounded.use(cfgRest) {
-          implicit CfgRest: Type[cfgRest.Underlying] =>
+          }
+        case ChimneyType.PatcherCfg.IgnoreNoneInPatch(cfgRest) =>
+          ExistentialType.Bounded.use(cfgRest) { implicit CfgRest: Type[cfgRest.Underlying] =>
             readPatcherConfigAux[cfgRest.Underlying](cfg).copy(ignoreNoneInPatch = true)
-        }
-      case ChimneyType.PatcherCfg.MacrosLogging(cfgRest) =>
-        ExistentialType.Bounded.use(cfgRest) {
-          implicit CfgRest: Type[cfgRest.Underlying] =>
+          }
+        case ChimneyType.PatcherCfg.MacrosLogging(cfgRest) =>
+          ExistentialType.Bounded.use(cfgRest) { implicit CfgRest: Type[cfgRest.Underlying] =>
             readPatcherConfigAux[cfgRest.Underlying](cfg).copy(displayMacrosLogging = true)
-        }
-      case _ =>
-        // $COVERAGE-OFF$
-        reportError(s"Bad internal patcher config type shape ${Type.prettyPrint[Cfg]}!")
+          }
+        case _ =>
+          // $COVERAGE-OFF$
+          reportError(s"Bad internal patcher config type shape ${Type.prettyPrint[Cfg]}!")
         // $COVERAGE-ON$
-    }
+      }
   }
 }
