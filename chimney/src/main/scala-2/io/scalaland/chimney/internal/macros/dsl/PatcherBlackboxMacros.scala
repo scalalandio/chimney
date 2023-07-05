43c05aed6d8bca1a564ccaffbb4ce351e991bcca
Piotr Krzemiński <pio.krzeminski@gmail.com>
2023-07-05 16:46:32 +0200

    comment out old PatcherBlackboxMacros

--- chimney/src/main/scala-2/io/scalaland/chimney/internal/macros/dsl/PatcherBlackboxMacros.scala
+++ chimney/src/main/scala-2/io/scalaland/chimney/internal/macros/dsl/PatcherBlackboxMacros.scala
@@ -1,20 +1,20 @@
-package io.scalaland.chimney.internal.macros.dsl
-
-import io.scalaland.chimney.Patcher
-import io.scalaland.chimney.internal.macros.PatcherMacros
-
-import scala.reflect.macros.blackbox
-
-// TODO: remove once patcher macros are migrated to new architecture
-class PatcherBlackboxMacros(val c: blackbox.Context) extends PatcherMacros {
-
-  import c.universe.*
-
-  def patchImpl[T: WeakTypeTag, Patch: WeakTypeTag, C: WeakTypeTag]: c.Expr[T] = {
-    c.Expr[T](expandPatch[T, Patch, C])
-  }
-
-  def derivePatcherImpl[T: WeakTypeTag, Patch: WeakTypeTag]: c.Expr[Patcher[T, Patch]] = {
-    genPatcher[T, Patch](PatcherConfig())
-  }
-}
+//package io.scalaland.chimney.internal.macros.dsl
+//
+//import io.scalaland.chimney.Patcher
+//import io.scalaland.chimney.internal.macros.PatcherMacros
+//
+//import scala.reflect.macros.blackbox
+//
+//// TODO: remove once patcher macros are migrated to new architecture
+//class PatcherBlackboxMacros(val c: blackbox.Context) extends PatcherMacros {
+//
+//  import c.universe.*
+//
+//  def patchImpl[T: WeakTypeTag, Patch: WeakTypeTag, C: WeakTypeTag]: c.Expr[T] = {
+//    c.Expr[T](expandPatch[T, Patch, C])
+//  }
+//
+//  def derivePatcherImpl[T: WeakTypeTag, Patch: WeakTypeTag]: c.Expr[Patcher[T, Patch]] = {
+//    genPatcher[T, Patch](PatcherConfig())
+//  }
+//}
