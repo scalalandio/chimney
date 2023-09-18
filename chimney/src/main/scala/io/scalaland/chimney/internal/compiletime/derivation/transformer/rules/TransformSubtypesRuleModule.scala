0b4277925c218e501315122ee0610218cd759d36
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2023-09-18 21:13:04 +0200

    Refactor rules: EitherToEither, Implicit, Subtypes and ProductToProduct

--- chimney/src/main/scala/io/scalaland/chimney/internal/compiletime/derivation/transformer/rules/TransformSubtypesRuleModule.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/compiletime/derivation/transformer/rules/TransformSubtypesRuleModule.scala
@@ -7,12 +7,19 @@ private[compiletime] trait TransformSubtypesRuleModule { this: Derivation =>
 
   protected object TransformSubtypesRule extends Rule("Subtypes") {
 
-    override def expand[From, To](implicit
+    def expand[From, To](implicit ctx: TransformationContext[From, To]): DerivationResult[Rule.ExpansionResult[To]] =
+      if (Type[From] <:< Type[To]) {
+        if (ctx.config.areOverridesEmpty) transformByUpcasting[From, To]
+        else
+          DerivationResult.log("Configuration has defined overrides - attempt to upcast source is skipped") >>
+            DerivationResult.attemptNextRule
+      } else DerivationResult.attemptNextRule
+
+    private def transformByUpcasting[From, To](implicit
         ctx: TransformationContext[From, To]
     ): DerivationResult[Rule.ExpansionResult[To]] =
       // We're constructing:
       // '{ ${ src } : $To } }
-      if (Type[From] <:< Type[To]) DerivationResult.expandedTotal(ctx.src.upcastExpr[To])
-      else DerivationResult.attemptNextRule
+      DerivationResult.expandedTotal(ctx.src.upcastExpr[To])
   }
 }
