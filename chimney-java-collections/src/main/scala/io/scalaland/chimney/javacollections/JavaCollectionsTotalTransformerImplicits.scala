ea21ebddf497f9b234d0aa509a22f722cdbfcfed
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2023-09-26 21:53:55 +0200

    Add total instances for Java Maps

--- chimney-java-collections/src/main/scala/io/scalaland/chimney/javacollections/JavaCollectionsTotalTransformerImplicits.scala
+++ chimney-java-collections/src/main/scala/io/scalaland/chimney/javacollections/JavaCollectionsTotalTransformerImplicits.scala
@@ -7,6 +7,8 @@ import scala.collection.compat.*
 /** @since 0.8.1 */
 trait JavaCollectionsTotalTransformerImplicits {
 
+  // non-Map collections
+
   /** @since 0.8.1 */
   implicit def totalTransformerFromJavaCollectionToScalaCollection[
       JColl[A0] <: java.lang.Iterable[A0],
@@ -52,7 +54,76 @@ trait JavaCollectionsTotalTransformerImplicits {
       builder.result()
     }
 
-  // TODO: Maps
+  // Maps
+
+  /** @since 0.8.1 */
+  implicit def totalTransformerFromJavaMapToScalaMap[
+      JMap[K0, V0] <: java.util.Map[K0, V0],
+      SMap[K0, V0] <: scala.collection.Map[K0, V0],
+      K1,
+      V1,
+      K2,
+      V2
+  ](implicit
+      keys: Transformer[K1, K2],
+      values: Transformer[V1, V2],
+      factory: Factory[(K2, V2), SMap[K2, V2]]
+  ): Transformer[JMap[K1, V1], SMap[K2, V2]] =
+    collection => {
+      val builder = factory.newBuilder
+      val it = collection.entrySet().iterator()
+      while (it.hasNext()) {
+        val entry = it.next()
+        builder += (keys.transform(entry.getKey) -> values.transform(entry.getValue))
+      }
+      builder.result()
+    }
+
+  /** @since 0.8.1 */
+  implicit def totalTransformerFromScalaMapToJavaMap[
+      SMap[K0, V0] <: scala.collection.Map[K0, V0],
+      JMap[K0, V0] <: java.util.Map[K0, V0],
+      K1,
+      V1,
+      K2,
+      V2
+  ](implicit
+      keys: Transformer[K1, K2],
+      values: Transformer[V1, V2],
+      factory: JavaFactory[(K2, V2), JMap[K2, V2]]
+  ): Transformer[SMap[K1, V1], JMap[K2, V2]] =
+    collection => {
+      val builder = factory.newBuilder
+      val it = collection.iterator
+      while (it.hasNext) {
+        val pair = it.next()
+        builder.addOne(keys.transform(pair._1) -> values.transform(pair._2))
+      }
+      builder.result()
+    }
+
+  /** @since 0.8.1 */
+  implicit def totalTransformerFromJavaMapToJavaMap[
+      JMap1[K0, V0] <: java.util.Map[K0, V0],
+      JMap2[K0, V0] <: java.util.Map[K0, V0],
+      K1,
+      V1,
+      K2,
+      V2
+  ](implicit
+      keys: Transformer[K1, K2],
+      values: Transformer[V1, V2],
+      factory: JavaFactory[(K2, V2), JMap2[K2, V2]]
+  ): Transformer[JMap1[K1, V1], JMap2[K2, V2]] =
+    collection => {
+      val builder = factory.newBuilder
+      val it = collection.entrySet().iterator()
+      while (it.hasNext()) {
+        val entry = it.next()
+        builder.addOne(keys.transform(entry.getKey) -> values.transform(entry.getValue))
+      }
+      builder.result()
+    }
 
   // TODO: Optionals
 
