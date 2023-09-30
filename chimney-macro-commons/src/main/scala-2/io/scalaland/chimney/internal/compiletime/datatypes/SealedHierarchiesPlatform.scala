3f7acbc22f16734b8cac65de94165cd49a17190c
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2023-09-30 13:25:50 +0200

    Start drafting support for java.util.Enum
    Borrows from https://github.com/scalalandio/chimney/pull/206 for Scala 2 implementation
    
    Co-authored-by: Dmytro Kostiuchenko <koiuo@127001.me>

--- chimney-macro-commons/src/main/scala-2/io/scalaland/chimney/internal/compiletime/datatypes/SealedHierarchiesPlatform.scala
+++ chimney-macro-commons/src/main/scala-2/io/scalaland/chimney/internal/compiletime/datatypes/SealedHierarchiesPlatform.scala
@@ -14,23 +14,33 @@ trait SealedHierarchiesPlatform extends SealedHierarchies { this: DefinitionsPla
       sym.isClass && sym.asClass.isSealed
     }
 
-    def parse[A: Type]: Option[Enum[A]] = if (isSealed(Type[A])) {
-      // calling .distinct here as `knownDirectSubclasses` returns duplicates for multiply-inherited types
-      val elements = extractSubclasses(Type[A].tpe.typeSymbol.asType).distinct
-        .map { (subtype: TypeSymbol) =>
-          val subtypeA = subtypeTypeOf[A](subtype)
-          subtypeA.mapK[Enum.Element[A, *]] { implicit Subtype: Type[subtypeA.Underlying] => _ =>
-            Enum.Element(name = subtypeName(subtype), upcast = _.upcastExpr[A])
-          }
-        }
-        // with GADT we can have subtypes that shouldn't appear in pattern matching
-        .filter(_.Underlying <:< Type[A])
-      Some(Enum(elements))
-    } else None
+    def isJavaEnum[A](A: Type[A]): Boolean = A.tpe.typeSymbol.isJavaEnum
+
+    def parse[A: Type]: Option[Enum[A]] =
+      if (isSealed(Type[A])) Some(symbolsToEnum(extractSubclasses(Type[A].tpe.typeSymbol.asType)))
+      else if (isJavaEnum(Type[A])) Some(symbolsToEnum(extractJavaEnumInstances(Type[A].tpe)))
+      else None
 
     private def extractSubclasses(t: TypeSymbol): List[TypeSymbol] =
       if (t.asClass.isSealed) t.asClass.knownDirectSubclasses.toList.map(_.asType).flatMap(extractSubclasses)
       else List(t)
+
+    private def extractJavaEnumInstances(t: c.Type): List[TypeSymbol] =
+      t.companion.decls.filter(_.isJavaEnum).map(_.asType).toList
+
+    private def symbolsToEnum[A: Type](subtypes: List[TypeSymbol]): Enum[A] =
+      Enum(
+        // calling .distinct here as `knownDirectSubclasses` returns duplicates for multiply-inherited types
+        subtypes.distinct
+          .map { (subtype: TypeSymbol) =>
+            val subtypeA = subtypeTypeOf[A](subtype)
+            subtypeA.mapK[Enum.Element[A, *]] { implicit Subtype: Type[subtypeA.Underlying] => _ =>
+              Enum.Element(name = subtypeName(subtype), upcast = _.upcastExpr[A])
+            }
+          }
+          // with GADT we can have subtypes that shouldn't appear in pattern matching
+          .filter(_.Underlying <:< Type[A])
+      )
 
     private def subtypeName(subtype: TypeSymbol): String = subtype.name.toString
   }
