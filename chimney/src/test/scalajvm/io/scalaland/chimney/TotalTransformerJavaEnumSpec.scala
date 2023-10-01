4f0ebb7aab9c3f743bbb36f2d813495d142a0468
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2023-10-01 11:11:24 +0200

    Enable remaining Java Enum tests

--- chimney/src/test/scalajvm/io/scalaland/chimney/TotalTransformerJavaEnumSpec.scala
+++ chimney/src/test/scalajvm/io/scalaland/chimney/TotalTransformerJavaEnumSpec.scala
@@ -4,12 +4,12 @@ import io.scalaland.chimney.dsl.*
 import io.scalaland.chimney.fixtures.*
 import io.scalaland.chimney.javafixtures.*
 
-//import scala.annotation.unused
+import scala.annotation.unused
 
 class TotalTransformerJavaEnumSpec extends ChimneySpec {
 
   test(
-    """transform flat sealed hierarchies from "subset" of case objects to "superset" of case objects without modifiers"""
+    """transform from Java Enum "subset" instances to sealed hierarchy "superset" of case objects without modifiers"""
   ) {
     (jcolors1.Color.Red: jcolors1.Color).transformInto[colors2.Color] ==> colors2.Red
     (jcolors1.Color.Green: jcolors1.Color).transformInto[colors2.Color] ==> colors2.Green
@@ -70,89 +70,44 @@ class TotalTransformerJavaEnumSpec extends ChimneySpec {
     )
   }
 
-//  group("setting .withCoproductInstance[Subtype](mapping)") {
-//
-//    test(
-//      """should be absent by default and not allow transforming "superset" of case class to "subset" of case objects"""
-//    ) {
-//      compileErrorsFixed("""(colors2.Black: colors2.Color).transformInto[colors1.Color]""").check(
-//        "Chimney can't derive transformation from io.scalaland.chimney.fixtures.colors2.Color to io.scalaland.chimney.fixtures.colors1.Color",
-//        "io.scalaland.chimney.fixtures.colors1.Color",
-//        "can't transform coproduct instance io.scalaland.chimney.fixtures.colors2.Black to io.scalaland.chimney.fixtures.colors1.Color",
-//        "Consult https://chimney.readthedocs.io for usage examples."
-//      )
-//    }
-//
-//    test(
-//      """transform sealed hierarchies from "superset" of case objects to "subset" of case objects when user-provided mapping handled additional cases"""
-//    ) {
-//      def blackIsRed(@unused b: colors2.Black.type): colors1.Color =
-//        colors1.Red
-//
-//      (colors2.Black: colors2.Color)
-//        .into[colors1.Color]
-//        .withCoproductInstance(blackIsRed)
-//        .transform ==> colors1.Red
-//
-//      (colors2.Red: colors2.Color)
-//        .into[colors1.Color]
-//        .withCoproductInstance(blackIsRed)
-//        .transform ==> colors1.Red
-//
-//      (colors2.Green: colors2.Color)
-//        .into[colors1.Color]
-//        .withCoproductInstance(blackIsRed)
-//        .transform ==> colors1.Green
-//
-//      (colors2.Blue: colors2.Color)
-//        .into[colors1.Color]
-//        .withCoproductInstance(blackIsRed)
-//        .transform ==> colors1.Blue
-//    }
-//
-//    test(
-//      """transform sealed hierarchies from "superset" of case classes to "subset" of case classes when user-provided mapping handled non-trivial cases"""
-//    ) {
-//      def triangleToPolygon(t: shapes1.Triangle): shapes2.Shape =
-//        shapes2.Polygon(
-//          List(
-//            t.p1.transformInto[shapes2.Point],
-//            t.p2.transformInto[shapes2.Point],
-//            t.p3.transformInto[shapes2.Point]
-//          )
-//        )
-//
-//      def rectangleToPolygon(r: shapes1.Rectangle): shapes2.Shape =
-//        shapes2.Polygon(
-//          List(
-//            r.p1.transformInto[shapes2.Point],
-//            shapes2.Point(r.p1.x, r.p2.y),
-//            r.p2.transformInto[shapes2.Point],
-//            shapes2.Point(r.p2.x, r.p1.y)
-//          )
-//        )
-//
-//      val triangle: shapes1.Shape =
-//        shapes1.Triangle(shapes1.Point(0, 0), shapes1.Point(2, 2), shapes1.Point(2, 0))
-//
-//      triangle
-//        .into[shapes2.Shape]
-//        .withCoproductInstance(triangleToPolygon)
-//        .withCoproductInstance(rectangleToPolygon)
-//        .transform ==> shapes2.Polygon(List(shapes2.Point(0, 0), shapes2.Point(2, 2), shapes2.Point(2, 0)))
-//
-//      val rectangle: shapes1.Shape =
-//        shapes1.Rectangle(shapes1.Point(0, 0), shapes1.Point(6, 4))
-//
-//      rectangle
-//        .into[shapes2.Shape]
-//        .withCoproductInstance[shapes1.Shape] {
-//          case r: shapes1.Rectangle => rectangleToPolygon(r)
-//          case t: shapes1.Triangle => triangleToPolygon(t)
-//        }
-//        .transform ==> shapes2.Polygon(
-//        List(shapes2.Point(0, 0), shapes2.Point(0, 4), shapes2.Point(6, 4), shapes2.Point(6, 0))
-//      )
-//    }
-//  }
+  group("setting .withCoproductInstance[Subtype](mapping)") {
+
+    test(
+      """should be absent by default and not allow transforming Java Enum "superset" instances to sealed hierarchy "subset" of case objects"""
+    ) {
+      compileErrorsFixed("""(jcolors2.Color.Black: jcolors2.Color).transformInto[colors1.Color]""").check(
+        "Chimney can't derive transformation from io.scalaland.chimney.javafixtures.jcolors2.Color to io.scalaland.chimney.fixtures.colors1.Color",
+        "io.scalaland.chimney.fixtures.colors1.Color",
+        "can't transform coproduct instance io.scalaland.chimney.javafixtures.jcolors2.Color.Black to io.scalaland.chimney.fixtures.colors1.Color",
+        "Consult https://chimney.readthedocs.io for usage examples."
+      )
+    }
+
+    test(
+      """transform from Java Enum "superset" instances to sealed hierarchy "subset" of case objects when user-provided mapping handled additional cases"""
+    ) {
+      def blackIsRed(@unused b: jcolors2.Color.Black.type): colors1.Color =
+        colors1.Red
+
+      (jcolors2.Color.Black: jcolors2.Color)
+        .into[colors1.Color]
+        .withCoproductInstance(blackIsRed)
+        .transform ==> colors1.Red
+
+      (jcolors2.Color.Red: jcolors2.Color)
+        .into[colors1.Color]
+        .withCoproductInstance(blackIsRed)
+        .transform ==> colors1.Red
+
+      (jcolors2.Color.Green: jcolors2.Color)
+        .into[colors1.Color]
+        .withCoproductInstance(blackIsRed)
+        .transform ==> colors1.Green
+
+      (jcolors2.Color.Blue: jcolors2.Color)
+        .into[colors1.Color]
+        .withCoproductInstance(blackIsRed)
+        .transform ==> colors1.Blue
+    }
+  }
 }
