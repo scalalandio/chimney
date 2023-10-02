a9f279525eb47732c430dc1fc1e4de63ac293d53
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2023-10-02 18:09:37 +0200

    Cleanup after adding java.lang.Enum support and better explanation why workarounds are needed

--- chimney/src/main/scala-2/io/scalaland/chimney/internal/runtime/RefinedJavaEnum.scala
+++ chimney/src/main/scala-2/io/scalaland/chimney/internal/runtime/RefinedJavaEnum.scala
@@ -1,11 +1,22 @@
 package io.scalaland.chimney.internal.runtime
 
-/** Apparently, `com.mypackage.JavaEnum.Value.type` is not a thing on Scala 2. At least when it comes to macros and
-  * `c.WeakTypeTag`.
+/** Apparently, `com.mypackage.JavaEnum.Value.type` is not a thing on Scala 2. This means that:
   *
-  * While there is `symbol.asTerm.typeSignature` for `JavaEnum.Value` (being aware that it "Value"), it will be upcasted
-  * to `JavaEnum`. We are only able to read it by reading a whole function tree, and we are only able to store it
-  * for DSL by encoding the name read from the tree in a dedicate type.
+  *   withCoproductInstance[com.mypackage.JavaEnum.Value.type] { value => ... }
+  *
+  * is ALWAYS treated as:
+  *
+  *   withCoproductInstance[com.mypackage.JavaEnum] { value => ... }
+  *
+  * matching on ALL values of `com.mypackage.JavaEnum`. Probably not what you want.
+  *
+  * While there is `symbol.asTerm.typeSignature` for `JavaEnum.Value` (being aware that it is "Value" and recognizing it
+  * in pattern-matching), it will be upcasted to `JavaEnum`. We are only able to read it by reading a whole function's
+  * tree:
+  *
+  *    withCoproductInstance { (value: com.mypackage.JavaEnum.Value.type) => ... }
+  *
+  * and we are only able to store it for DSL by encoding the name read from the tree with a dedicated type from below.
   *
   * In Scala 3 no such shenanigan are needed.
   */
