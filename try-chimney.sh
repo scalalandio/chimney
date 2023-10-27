bbd306e2219db128ade77ee290e66ecba87b44c9
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2023-10-27 19:49:30 +0200

    Make try-chimney.sh script always using newest version

--- try-chimney.sh
+++ try-chimney.sh
@@ -3,9 +3,5 @@ COURSIER_URL=https://github.com/coursier/launchers/raw/master/coursier
 
 test -e ~/.coursier/coursier || (mkdir -p ~/.coursier && curl -fLo ~/.coursier/coursier $COURSIER_URL && chmod +x ~/.coursier/coursier)
 
-~/.coursier/coursier launch -q -P -M ammonite.Main \
-  com.lihaoyi:ammonite_2.13.3:2.5.9 \
-  org.typelevel:cats-core_2.13:2.9.0 \
-  io.scalaland:chimney_2.13:0.8.0 \
-  io.scalaland:chimney-cats_2.13:0.8.0 \
-  -- --predef-code 'import $plugin.$ivy.`org.typelevel:kind-projector_2.13.10:0.13.2`;import io.scalaland.chimney.dsl._;import io.scalaland.chimney.cats._' < /dev/tty
+~/.coursier/coursier launch --java-opt -Dfoo=bar --java-opt -Xmx2g --java-opt -Xss16m com.lihaoyi:ammonite_3.3.1:latest.release -M ammonite.Main \
+  -- --predef-code 'import $ivy.`io.scalaland::chimney-cats:latest.release`;import io.scalaland.chimney.dsl._;import io.scalaland.chimney.cats._' < /dev/tty
