8a3cb3c320d79600176e2cda161ede2f4171da87
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-01-10 10:42:38 +0100

    Fix Cats' conversions tests

--- chimney-cats/src/test/scala/io/scalaland/chimney/cats/PartialResultToCatsDataConversionSpec.scala
+++ chimney-cats/src/test/scala/io/scalaland/chimney/cats/PartialResultToCatsDataConversionSpec.scala
@@ -2,73 +2,58 @@ package io.scalaland.chimney.cats
 
 import _root_.cats.data.{Chain, NonEmptyChain, NonEmptyList, Validated}
 import _root_.cats.syntax.validated.*
-import _root_.cats.syntax.semigroup.*
-import _root_.cats.syntax.semigroupal.*
-import cats.Semigroupal
 import io.scalaland.chimney.{partial, ChimneySpec}
 import io.scalaland.chimney.partial.{Error, PathElement}
 import io.scalaland.chimney.dsl.*
 import io.scalaland.chimney.fixtures.trip.*
 
-class PartialTransformerToCatsDataConversionSpec extends ChimneySpec {
-
-  test("conversion from partial.Result to Validated") {
-
-    val e1 = partial.Result.Errors.fromString("test1")
-    val e2 = partial.Result.Errors.fromString("test2")
-
-    e1.combine(e2) ==> partial.Result.Errors.fromStrings("test1", "test2")
-  }
-
-  group("partial transformer result semigroupal instance") {
-
-    test("success") {
-      partial.Result
-        .fromValue(1)
-        .product(partial.Result.fromValue("abc")) ==>
-        partial.Result.fromValue((1, "abc"))
-    }
-
-    test("failure") {
-      Semigroupal[partial.Result].product(
-        partial.Result.fromValue(1),
-        partial.Result.fromErrorString("abc")
-      ) ==>
-        partial.Result.fromErrorString("abc")
-
-      Semigroupal[partial.Result].product(
-        partial.Result.fromErrorString("abc"),
-        partial.Result.fromValue(1)
-      ) ==>
-        partial.Result.fromErrorString("abc")
-
-      Semigroupal[partial.Result].product(
-        partial.Result.fromErrorString("abc"),
-        partial.Result.fromErrorString("def")
-      ) ==>
-        partial.Result.fromErrorStrings("abc", "def")
-    }
-  }
+class PartialResultToCatsDataConversionSpec extends ChimneySpec {
 
   group("conversion between Validated and partial.Result") {
 
-    test("successful Result should convert to Valid") {
-      Person("John", 10, 140).transformIntoPartial[User].asValidated ==> Validated.valid(User("John", 10, 140))
-      Person("John", 10, 140).transformIntoPartial[User].asValidatedNel ==> Validated.validNel(User("John", 10, 140))
-      Person("John", 10, 140).transformIntoPartial[User].asValidatedNec ==> Validated.validNec(User("John", 10, 140))
-      Person("John", 10, 140).transformIntoPartial[User].asValidatedList ==> Validated.valid(User("John", 10, 140))
-      Person("John", 10, 140).transformIntoPartial[User].asValidatedChain ==> Validated.valid(User("John", 10, 140))
-
-      Person("John", 10, 140).intoPartial[User].transform.asValidated ==> Validated.valid(User("John", 10, 140))
-      Person("John", 10, 140).intoPartial[User].transform.asValidatedNel ==> Validated.validNel(User("John", 10, 140))
-      Person("John", 10, 140).intoPartial[User].transform.asValidatedNec ==> Validated.validNec(User("John", 10, 140))
-      Person("John", 10, 140).intoPartial[User].transform.asValidatedList ==> Validated.valid(User("John", 10, 140))
-      Person("John", 10, 140).intoPartial[User].transform.asValidatedChain ==> Validated.valid(User("John", 10, 140))
+    test("Valid should convert to partial.Result.Valid") {
+      Validated.valid[partial.Result.Errors, User](User("John", 10, 140)).toPartialResult ==>
+        partial.Result.fromValue(User("John", 10, 140))
+      Validated.validNec[partial.Error, User](User("John", 10, 140)).toPartialResult ==>
+        partial.Result.fromValue(User("John", 10, 140))
+      Validated.validNec[String, User](User("John", 10, 140)).toPartialResult ==>
+        partial.Result.fromValue(User("John", 10, 140))
+      Validated.validNel[partial.Error, User](User("John", 10, 140)).toPartialResult ==>
+        partial.Result.fromValue(User("John", 10, 140))
+      Validated.validNel[String, User](User("John", 10, 140)).toPartialResult ==>
+        partial.Result.fromValue(User("John", 10, 140))
     }
 
-    group("failed Result should convert to Invalid") {
+    test("Invalid should convert to partial.Result.Errors") {
+      Validated.invalid[partial.Result.Errors, User](partial.Result.Errors.fromString("error")).toPartialResult ==>
+        partial.Result.fromErrorString("error")
+      Validated.invalidNec[partial.Error, User](partial.Error.fromString("error")).toPartialResult ==>
+        partial.Result.fromErrorString("error")
+      Validated.invalidNec[String, User]("error").toPartialResult ==>
+        partial.Result.fromErrorString("error")
+      Validated.invalidNel[partial.Error, User](partial.Error.fromString("error")).toPartialResult ==>
+        partial.Result.fromErrorString("error")
+      Validated.invalidNel[String, User]("error").toPartialResult ==>
+        partial.Result.fromErrorString("error")
+    }
 
-      test("String errors") {
+    test("partial.Result.Valid should convert to Valid") {
+      Person("John", 10, 140).transformIntoPartial[User].asValidated ==> Validated.valid(User("John", 10, 140))
+      Person("John", 10, 140).transformIntoPartial[User].asValidatedNec ==> Validated.validNec(User("John", 10, 140))
+      Person("John", 10, 140).transformIntoPartial[User].asValidatedNel ==> Validated.validNel(User("John", 10, 140))
+      Person("John", 10, 140).transformIntoPartial[User].asValidatedChain ==> Validated.valid(User("John", 10, 140))
+      Person("John", 10, 140).transformIntoPartial[User].asValidatedList ==> Validated.valid(User("John", 10, 140))
+
+      Person("John", 10, 140).intoPartial[User].transform.asValidated ==> Validated.valid(User("John", 10, 140))
+      Person("John", 10, 140).intoPartial[User].transform.asValidatedNec ==> Validated.validNec(User("John", 10, 140))
+      Person("John", 10, 140).intoPartial[User].transform.asValidatedNel ==> Validated.validNel(User("John", 10, 140))
+      Person("John", 10, 140).intoPartial[User].transform.asValidatedChain ==> Validated.valid(User("John", 10, 140))
+      Person("John", 10, 140).intoPartial[User].transform.asValidatedList ==> Validated.valid(User("John", 10, 140))
+    }
+
+    group("partial.Result.Errors should convert to Invalid") {
+
+      test("for String errors") {
         val result = Person("John", 10, 140)
           .intoPartial[User]
           .withFieldConstPartial(_.name, NonEmptyChain.of("foo").invalid.toPartialResult)
@@ -83,13 +68,13 @@ class PartialTransformerToCatsDataConversionSpec extends ChimneySpec {
         result.asValidated ==> Validated.invalid(
           partial.Result.fromErrors(expectedErr1, expectedErr2, expectedErr3)
         )
-        result.asValidatedNel ==> Validated.invalid(NonEmptyList.of(expectedErr1, expectedErr2, expectedErr3))
         result.asValidatedNec ==> Validated.invalid(NonEmptyChain.of(expectedErr1, expectedErr2, expectedErr3))
-        result.asValidatedList ==> Validated.invalid(List(expectedErr1, expectedErr2, expectedErr3))
+        result.asValidatedNel ==> Validated.invalid(NonEmptyList.of(expectedErr1, expectedErr2, expectedErr3))
         result.asValidatedChain ==> Validated.invalid(Chain(expectedErr1, expectedErr2, expectedErr3))
+        result.asValidatedList ==> Validated.invalid(List(expectedErr1, expectedErr2, expectedErr3))
       }
 
-      test("Throwable errors") {
+      test("for Throwable errors") {
 
         val ex1 = new RuntimeException("foo")
         val ex2 = new RuntimeException("abc")
@@ -118,10 +103,10 @@ class PartialTransformerToCatsDataConversionSpec extends ChimneySpec {
         result.asValidated ==> Validated.invalid(
           partial.Result.fromErrors(expectedErr1, expectedErr2, expectedErr3)
         )
-        result.asValidatedNel ==> Validated.invalid(NonEmptyList.of(expectedErr1, expectedErr2, expectedErr3))
         result.asValidatedNec ==> Validated.invalid(NonEmptyChain.of(expectedErr1, expectedErr2, expectedErr3))
-        result.asValidatedList ==> Validated.invalid(List(expectedErr1, expectedErr2, expectedErr3))
+        result.asValidatedNel ==> Validated.invalid(NonEmptyList.of(expectedErr1, expectedErr2, expectedErr3))
         result.asValidatedChain ==> Validated.invalid(Chain(expectedErr1, expectedErr2, expectedErr3))
+        result.asValidatedList ==> Validated.invalid(List(expectedErr1, expectedErr2, expectedErr3))
       }
     }
   }
