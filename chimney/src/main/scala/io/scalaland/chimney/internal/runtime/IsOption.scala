131a508c1c0e7def3cdd34cdb9b8ed48c607f240
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-04-22 22:33:20 +0200

    Add OptionalValue to IsOption evidence

--- chimney/src/main/scala/io/scalaland/chimney/internal/runtime/IsOption.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/runtime/IsOption.scala
@@ -1,6 +1,8 @@
 package io.scalaland.chimney.internal.runtime
 
-import scala.annotation.implicitNotFound
+import io.scalaland.chimney.integrations.OptionalValue
+
+import scala.annotation.{implicitNotFound, unused}
 
 // $COVERAGE-OFF$evidence used only within macro-erased expressions
 
@@ -8,20 +10,25 @@ import scala.annotation.implicitNotFound
   *
   * @since 1.0.0
   */
-@implicitNotFound("Expected Option (type extending scala.Option), got ${O}")
+@implicitNotFound("Expected Option (type extending scala.Option) or OptionalValue, got ${O}")
 sealed trait IsOption[O] {
   type SomeValue
   type Some
 }
-object IsOption {
-  @implicitNotFound("Expected Option (type extending scala.Option), got ${O}")
+object IsOption extends IsOptionLowPriority {
+  @implicitNotFound("Expected Option (type extending scala.Option) or OptionalValue, got ${O}")
   type Of[O, SV, S] = IsOption[O] { type SomeValue = SV; type Some = S }
 
-  private object Impl extends IsOption[Nothing]
+  protected object Impl extends IsOption[Nothing]
 
   implicit def optionIsOption[A]: IsOption.Of[Option[A], A, Some[A]] =
     Impl.asInstanceOf[IsOption.Of[Option[A], A, Some[A]]]
 
   implicit def someIsOption[A]: IsOption.Of[Some[A], A, Some[A]] =
     Impl.asInstanceOf[IsOption.Of[Some[A], A, Some[A]]]
+}
+private[runtime] trait IsOptionLowPriority { this: IsOption.type =>
+
+  implicit def optionalValueIsOption[O, A](implicit @unused ev: OptionalValue[O, A]): IsOption.Of[O, A, Some[A]] =
+    Impl.asInstanceOf[IsOption.Of[O, A, Some[A]]]
 }
