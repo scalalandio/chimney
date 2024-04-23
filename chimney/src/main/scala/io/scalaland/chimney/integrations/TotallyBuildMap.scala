e2f166c6168fcc150eba732ea2ae0e0f1ca5347a
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-04-24 00:46:37 +0200

    Add scaladocs for integrations

--- chimney/src/main/scala/io/scalaland/chimney/integrations/TotallyBuildMap.scala
+++ chimney/src/main/scala/io/scalaland/chimney/integrations/TotallyBuildMap.scala
@@ -1,3 +1,17 @@
 package io.scalaland.chimney.integrations
 
+/** Tells Chimney how to interact with `Collection` type to align its behavior with e.g. [[Map]].
+  *
+  * @see
+  *   [[https://chimney.readthedocs.io/cookbook/#custom-collection-types]] for more details
+  *
+  * @tparam Mapp
+  *   type storing collection of `Key`-`Value` pairs - has to be proper type, not higher-kinded type
+  * @tparam Key
+  *   type of internal keys
+  * @tparam Value
+  *   type of internal values
+  *
+  * @since 1.0.0
+  */
 trait TotallyBuildMap[Mapp, Key, Value] extends TotallyBuildIterable[Mapp, (Key, Value)]
