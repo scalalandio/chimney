c722db3d0ddb4670d57baf1c6ff9a0274bbedbd4
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-06-25 16:41:47 +0200

    Test flags in Codec/Iso

--- chimney/src/test/scala/io/scalaland/chimney/IsoProductSpec.scala
+++ chimney/src/test/scala/io/scalaland/chimney/IsoProductSpec.scala
@@ -5,19 +5,29 @@ import io.scalaland.chimney.fixtures.*
 
 class IsoProductSpec extends ChimneySpec {
 
-  group("""setting .withFieldRenamed(_.from, _.to)""") {
+  test("""setting .withFieldRenamed(_.from, _.to) should be correctly forwarded to Transformers""") {
 
-    test("should be correctly forwarded to Transformers") {
-      import products.Renames.{User, UserPLStd}
+    import products.Renames.{User, UserPLStd}
 
-      implicit val iso: Iso[User, UserPLStd] = Iso
-        .define[User, UserPLStd]
-        .withFieldRenamed(_.name, _.imie)
-        .withFieldRenamed(_.age, _.wiek)
-        .buildIso
+    implicit val iso: Iso[User, UserPLStd] = Iso
+      .define[User, UserPLStd]
+      .withFieldRenamed(_.name, _.imie)
+      .withFieldRenamed(_.age, _.wiek)
+      .buildIso
 
-      User(1, "John", Some(27)).transformInto[UserPLStd] ==> UserPLStd(1, "John", Some(27))
-      UserPLStd(1, "John", Some(27)).transformInto[User] ==> User(1, "John", Some(27))
-    }
+    User(1, "John", Some(27)).transformInto[UserPLStd] ==> UserPLStd(1, "John", Some(27))
+    UserPLStd(1, "John", Some(27)).transformInto[User] ==> User(1, "John", Some(27))
+  }
+
+  test("""flags should be correctly forwarded to Transformers""") {
+    import products.Defaults.{Target, Target2}
+
+    implicit val iso: Iso[Target, Target2] = Iso
+      .define[Target, Target2]
+      .enableDefaultValues
+      .buildIso
+
+    Target(1, "n", 7.7).transformInto[Target2] ==> Target2(10, "y", 7.7)
+    Target2(1, "n", 7.7).transformInto[Target] ==> Target(10, "y", 7.7)
   }
 }
