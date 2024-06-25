3c45306c104edaff78d12ecbe7b81b78ae52932f
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-06-25 16:10:14 +0200

    Make sure Codec/Iso tests aren't deriving Transformers on the spot

--- chimney/src/test/scala/io/scalaland/chimney/CodecImplicitResolutionSpec.scala
+++ chimney/src/test/scala/io/scalaland/chimney/CodecImplicitResolutionSpec.scala
@@ -7,22 +7,26 @@ class CodecImplicitResolutionSpec extends ChimneySpec {
 
   test("encode using Total Transformer from Codec when available") {
     import products.Domain1.*
-    implicit def usernameToString: Transformer[UserName, String] =
-      userNameToStringTransformer
-    implicit def stringToUsername: PartialTransformer[String, UserName] =
-      PartialTransformer.liftTotal(value => UserName(value))
-    implicit def codec: Codec[User, UserDTO] = Codec.derive
+    implicit def codec: Codec[User, UserDTO] = locally {
+      implicit def usernameToString: Transformer[UserName, String] =
+        userNameToStringTransformer
+      implicit def stringToUsername: PartialTransformer[String, UserName] =
+        PartialTransformer.liftTotal(value => UserName(value))
+      Codec.derive
+    }
 
     User("1", UserName("name")).transformInto[UserDTO] ==> UserDTO("1", "nameT")
   }
 
   test("decode using Partial Transformer from Codec when available") {
     import products.Domain1.*
-    implicit def usernameToString: Transformer[UserName, String] =
-      userNameToStringTransformer
-    implicit def stringToUsername: PartialTransformer[String, UserName] =
-      PartialTransformer.liftTotal(value => UserName(value + "T"))
-    implicit def codec: Codec[User, UserDTO] = Codec.derive
+    implicit def codec: Codec[User, UserDTO] = locally {
+      implicit def usernameToString: Transformer[UserName, String] =
+        userNameToStringTransformer
+      implicit def stringToUsername: PartialTransformer[String, UserName] =
+        PartialTransformer.liftTotal(value => UserName(value + "T"))
+      Codec.derive
+    }
 
     UserDTO("1", "name").transformIntoPartial[User].asOption ==> Some(User("1", UserName("nameT")))
   }
