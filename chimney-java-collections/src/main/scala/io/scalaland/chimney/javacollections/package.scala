d8f0bec08b17fe9d7bbbb15f7f6dcd8937bf102d
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-06-25 07:47:02 +0200

    Add conversion to and from Java primitives

--- chimney-java-collections/src/main/scala/io/scalaland/chimney/javacollections/package.scala
+++ chimney-java-collections/src/main/scala/io/scalaland/chimney/javacollections/package.scala
@@ -1,4 +1,4 @@
 package io.scalaland.chimney
 
 /** @since 0.8.0 */
-package object javacollections extends JavaCollectionsImplicits
+package object javacollections extends JavaCollectionsImplicits with JavaPrimitivesImplicits
