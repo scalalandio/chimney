74ab51f2ba5b53f3fe9bb28145f1a417966292de
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-06-25 16:10:14 +0200

    Make sure Codec/Iso tests aren't deriving Transformers on the spot

--- chimney/src/test/scala/io/scalaland/chimney/IsoImplicitResolutionSpec.scala
+++ chimney/src/test/scala/io/scalaland/chimney/IsoImplicitResolutionSpec.scala
@@ -7,20 +7,24 @@ class IsoImplicitResolutionSpec extends ChimneySpec {
 
   test("convert using the first Total Transformer from Iso when available") {
     import products.Domain1.*
-    implicit def usernameToString: Transformer[UserName, String] =
-      userNameToStringTransformer
-    implicit def stringToUsername: Transformer[String, UserName] = value => UserName(value + "T")
-    implicit def iso: Iso[User, UserDTO] = Iso.derive
+    implicit def iso: Iso[User, UserDTO] = locally {
+      implicit def usernameToString: Transformer[UserName, String] =
+        userNameToStringTransformer
+      implicit def stringToUsername: Transformer[String, UserName] = value => UserName(value + "T")
+      Iso.derive
+    }
 
     User("1", UserName("name")).transformInto[UserDTO] ==> UserDTO("1", "nameT")
   }
 
   test("convert using the second Total Transformer from Iso when available") {
     import products.Domain1.*
-    implicit def usernameToString: Transformer[UserName, String] =
-      userNameToStringTransformer
-    implicit def stringToUsername: Transformer[String, UserName] = value => UserName(value + "T")
-    implicit def iso: Iso[User, UserDTO] = Iso.derive
+    implicit def iso: Iso[User, UserDTO] = locally {
+      implicit def usernameToString: Transformer[UserName, String] =
+        userNameToStringTransformer
+      implicit def stringToUsername: Transformer[String, UserName] = value => UserName(value + "T")
+      Iso.derive
+    }
 
     UserDTO("1", "name").transformIntoPartial[User].asOption ==> Some(User("1", UserName("nameT")))
   }
