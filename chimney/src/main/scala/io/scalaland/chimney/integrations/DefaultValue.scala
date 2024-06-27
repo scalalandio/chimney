7bfd263e015aa2cd50d3070b4b9fdfc0ae64e7d8
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-06-27 11:50:35 +0200

    Add documentation for DefaultValueForType flag and DefaultValue type class, fix a few overlooked TODOs

--- chimney/src/main/scala/io/scalaland/chimney/integrations/DefaultValue.scala
+++ chimney/src/main/scala/io/scalaland/chimney/integrations/DefaultValue.scala
@@ -3,7 +3,7 @@ package io.scalaland.chimney.integrations
 /** Tells Chimney how to provide default value of type `Value` if flag allows them but field does not define it.
   *
   * @see
-  *   [[https://chimney.readthedocs.io/cookbook/#custom-default-values]] for more details TODO
+  *   [[https://chimney.readthedocs.io/cookbook/#custom-default-values]] for more details
   *
   * @tparam Value
   *   type of default value
