f241d30f00de26e5ea6ce54334605500d8d8414b
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-07-04 08:37:59 +0200

    Update FIXME note to protobufs, fix typo and add OSS Snapshot info in CONTRIBUTORS.md

--- chimney-protobufs/src/main/scala/io/scalaland/chimney/protobufs/ProtobufTransformerImplicits.scala
+++ chimney-protobufs/src/main/scala/io/scalaland/chimney/protobufs/ProtobufTransformerImplicits.scala
@@ -6,7 +6,7 @@ import io.scalaland.chimney.Transformer
 import scala.collection.compat._
 // format: on
 
-// FIXME: when we would do some 2.0.0 release, we should align the naming (Protobuf -> Protobufs)
+// FIXME (2.0.0 cleanup): we should align the naming (Protobuf -> Protobufs)
 
 /** @since 0.8.0 */
 trait ProtobufTransformerImplicits extends ProtobufTransformerImplicitsLowPriorityImplicits1 {}
