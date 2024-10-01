6cddfa865885e050b5cf27489b6acb370bd4bc6a
Naeim Taheri <nima.trueway@gmail.com>
2024-09-30 19:47:23 -0700

    Revert "feat: add PathList to support vararg macros"
    This reverts commit 333f10562698042317e734d25392d12182113c6c.

--- chimney/src/main/scala/io/scalaland/chimney/internal/runtime/PathList.scala
+++
@@ -1,7 +0,0 @@
-package io.scalaland.chimney.internal.runtime
-
-sealed abstract class PathList
-object PathList {
-  final class Empty extends PathList
-  final class List[Head <: Path, Tail <: PathList] extends PathList
-}
