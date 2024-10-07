eadc72b3ca2bd70c304244a5dc16345dc985564c
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-10-07 07:55:19 +0200

    Port DefCache from derivation-benchmarks

--- chimney-macro-commons/src/main/scala/io/scalaland/chimney/internal/compiletime/ExprPromises.scala
+++ chimney-macro-commons/src/main/scala/io/scalaland/chimney/internal/compiletime/ExprPromises.scala
@@ -1,5 +1,7 @@
 package io.scalaland.chimney.internal.compiletime
 
+import scala.collection.immutable.ListMap
+
 private[compiletime] trait ExprPromises { this: Definitions =>
 
   /** In Scala 2 it's `c.universe.TermName`, in Scala 3 `Symbol` of a val */
@@ -255,5 +257,119 @@ private[compiletime] trait ExprPromises { this: Definitions =>
   implicit final protected class ListPatternMatchCaseOps[To: Type](cases: List[PatternMatchCase[To]]) {
 
     def matchOn[From: Type](src: Expr[From]): Expr[To] = PatternMatchCase.matchOn(src, cases)
+  }
+
+  /** Allows "caching" some `Expr[A] => F[Expr[B]]` derivation as a `def`.
+    *
+    * @tparam F
+    *   type of the result that would be cached method's body (including failed derivation)
+    */
+  abstract protected class DefCache[F[_]: fp.DirectStyle] {
+    // Soo... neither a =:= b nor Type.isSameAs(a, b) want to work with Type[?] :/
+    implicit private class TypeAnyOps[A](private val tpe: Type[A]) {
+      def asAny: Type[Any] = tpe.asInstanceOf[Type[Any]]
+    }
+
+    // Key of the internal map of derived bodies
+    private case class Signature(name: String, input: List[Type[Any]], output: Type[Any]) {
+
+      override def hashCode(): Int = name.hashCode
+
+      override def equals(obj: Any): Boolean = obj match {
+        case Signature(name2, input2, output2) =>
+          name == name2 && input.length == input2.length && input
+            .zip(input2)
+            .forall(p => p._1 =:= p._2) && output =:= output2
+        case _ => false
+      }
+
+      override def toString: String =
+        s"def $name(${input.map(Type.prettyPrint(_)).mkString(", ")}): ${Type.prettyPrint(output)}"
+    }
+    private object Signature {
+      def apply[In1: Type, Out: Type](name: String) =
+        new Signature(name, List(Type[In1].asAny), Type[Out].asAny)
+      def apply[In1: Type, In2: Type, Out: Type](name: String) =
+        new Signature(name, List(Type[In1].asAny, Type[In2].asAny), Type[Out].asAny)
+    }
+
+    // Allows accessing def inside its body
+    protected trait PendingDef {
+      def cast[A]: A
+    }
+
+    // Allows accessing def once it's defined
+    protected trait Def {
+      def prependDef[A: Type](expr: Expr[A]): Expr[A]
+      def cast[A]: A
+    }
+
+    // Platform-specific way of creating def out of its body
+    protected trait Define[In, Out] {
+      def apply(body: In => Out): Def
+      def pending: PendingDef
+    }
+
+    private var pending = ListMap.empty[Signature, PendingDef]
+    private var defined = ListMap.empty[Signature, Def]
+
+    private def unsafeGet[A](signature: Signature): Option[A] =
+      defined.get(signature).map(_.cast[A]).orElse(pending.get(signature).map(_.cast[A]))
+
+    final class Builder[In, Out, A] private (
+        private val signature: Signature,
+        private val body: In => A,
+        private val define: Define[In, Out]
+    ) {
+      def map[B](f: A => B): Builder[In, Out, B] =
+        new Builder[In, Out, B](signature, body andThen f, define)
+      def emap[B](f: A => F[B]): Builder[In, Out, B] =
+        new Builder[In, Out, B](signature, body andThen f andThen fp.DirectStyle[F].awaitUnsafe, define)
+      def build(implicit ev: (In => A) =:= (In => Out)): Unit = {
+        val evaluated = define(ev(body))
+        pending = pending - signature
+        defined = defined + (signature -> evaluated)
+      }
+    }
+    private object Builder {
+      def apply[In, Out](signature: Signature, define: Define[In, Out]): Builder[In, Out, In] = {
+        val evaluated = define.pending
+        pending = pending + (signature -> evaluated)
+        new Builder[In, Out, In](signature, identity[In](_), define)
+      }
+    }
+
+    final def build1[In1: Type, Out: Type](name: String): Builder[Expr[In1], Expr[Out], Expr[In1]] =
+      Builder(Signature[In1, Out](name), define1[In1, Out](name))
+    final def build2[In1: Type, In2: Type, Out: Type](
+        name: String
+    ): Builder[(Expr[In1], Expr[In2]), Expr[Out], (Expr[In1], Expr[In2])] =
+      Builder(Signature[In1, In2, Out](name), define2[In1, In2, Out](name))
+
+    final def of1[In1: Type, Out: Type](name: String): Option[Expr[In1] => F[Expr[Out]]] =
+      unsafeGet[Expr[In1] => Expr[Out]](Signature[In1, Out](name)).map(fn =>
+        in1 => fp.DirectStyle[F].asyncUnsafe(fn(in1))
+      )
+    final def of2[In1: Type, In2: Type, Out: Type](name: String): Option[(Expr[In1], Expr[In2]) => F[Expr[Out]]] =
+      unsafeGet[(Expr[In1], Expr[In2]) => Expr[Out]](Signature[In1, In2, Out](name)).map(fn =>
+        (in1, in2) => fp.DirectStyle[F].asyncUnsafe(fn(in1, in2))
+      )
+
+    final def prependDefs[A: Type](expr: Expr[A]): F[Expr[A]] = fp.DirectStyle[F].asyncUnsafe {
+      defined.values.foldRight(expr)((def0, e) => def0.prependDef(e))
+    }
+
+    // Platform-specific implementations
+    protected def define1[In1: Type, Out: Type](name: String): Define[Expr[In1], Expr[Out]]
+    protected def define2[In1: Type, In2: Type, Out: Type](name: String): Define[(Expr[In1], Expr[In2]), Expr[Out]]
+
+    override def toString: String =
+      s"DefCache(pending = Seq(${pending.keys.mkString(", ")}), defined = Seq(${defined.keys.mkString(", ")}))"
+  }
+
+  protected val DefCache: DefCacheModule
+  protected trait DefCacheModule { this: DefCache.type =>
+
+    def apply[F[_]: fp.DirectStyle]: DefCache[F]
   }
 }
