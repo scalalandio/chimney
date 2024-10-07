d33b3ac03f1e9898a85bf64a5240db00e1900325
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-10-07 22:30:16 +0200

    DSL for OuterTransformers

--- chimney/src/main/scala/io/scalaland/chimney/internal/runtime/IsCollection.scala
+++ chimney/src/main/scala/io/scalaland/chimney/internal/runtime/IsCollection.scala
@@ -27,24 +27,38 @@ object IsCollection extends IsCollectionImplicits0 with IsCollectionImplicitComp
 }
 private[runtime] trait IsCollectionImplicits0 extends IsCollectionImplicits1 { this: IsCollection.type =>
 
+  // outer transformers should have a priority over collections
+  implicit def totalOuterTransformerIsCollection[To, InnerTo](implicit
+      @unused ev: TotalOuterTransformer[?, To, ?, InnerTo]
+  ): IsCollection.Of[To, InnerTo] = Impl.asInstanceOf[IsCollection.Of[To, InnerTo]]
+}
+private[runtime] trait IsCollectionImplicits1 extends IsCollectionImplicits2 { this: IsCollection.type =>
+
+  // outer transformers should have a priority over collections
+  implicit def partialOuterTransformerIsCollection[To, InnerTo](implicit
+      @unused ev: PartialOuterTransformer[?, To, ?, InnerTo]
+  ): IsCollection.Of[To, InnerTo] = Impl.asInstanceOf[IsCollection.Of[To, InnerTo]]
+}
+private[runtime] trait IsCollectionImplicits2 extends IsCollectionImplicits3 { this: IsCollection.type =>
+
   // build-in Chimney support for Arrays is always provided
   implicit def arrayIsCollection[A]: IsCollection.Of[Array[A], A] = Impl.asInstanceOf[IsCollection.Of[Array[A], A]]
 }
-private[runtime] trait IsCollectionImplicits1 extends IsCollectionImplicits2 { this: IsCollection.type =>
+private[runtime] trait IsCollectionImplicits3 extends IsCollectionImplicits4 { this: IsCollection.type =>
 
   // build-in Chimney support for collections assumes that they are BOTH Iterable and have a Factory
   implicit def scalaCollectionIsCollection[A, C <: Iterable[A]](implicit
       @unused ev: Factory[A, C]
   ): IsCollection.Of[C, A] = Impl.asInstanceOf[IsCollection.Of[C, A]]
 }
-private[runtime] trait IsCollectionImplicits2 extends IsCollectionImplicits3 { this: IsCollection.type =>
+private[runtime] trait IsCollectionImplicits4 extends IsCollectionImplicits5 { this: IsCollection.type =>
 
   // TotallyBuildIterable is supported by design
   implicit def totallyBuildIterableIsCollection[A, C](implicit
       @unused ev: TotallyBuildIterable[C, A]
   ): IsCollection.Of[C, A] = Impl.asInstanceOf[IsCollection.Of[C, A]]
 }
-private[runtime] trait IsCollectionImplicits3 { this: IsCollection.type =>
+private[runtime] trait IsCollectionImplicits5 { this: IsCollection.type =>
 
   // PartiallyBuildIterable is supported by design
   implicit def partiallyBuildIterableIsCollection[A, C](implicit
