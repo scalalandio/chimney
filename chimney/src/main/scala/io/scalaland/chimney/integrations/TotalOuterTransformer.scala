ebe27d264a41ffefba2bc47246b4ffaf27c73cda
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-10-08 17:21:23 +0200

    Document Outer Transformers

--- chimney/src/main/scala/io/scalaland/chimney/integrations/TotalOuterTransformer.scala
+++ chimney/src/main/scala/io/scalaland/chimney/integrations/TotalOuterTransformer.scala
@@ -2,13 +2,29 @@ package io.scalaland.chimney.integrations
 
 import io.scalaland.chimney.partial
 
+/** Tells Chimney how to convert some outer types/wrappers/collections where conversions for their inner values/items
+  * could be derived, when the type can be always constructed.
+  *
+  * @tparam From
+  *   whole outer source type (with all possible type parameters applied)
+  * @tparam To
+  *   whole outer target type (with all possible type parameters applied)
+  * @tparam InnerFrom
+  *   type of the value(s) inside From that Chimney can derive conversion from
+  * @tparam InnerTo
+  *   type of the value(s) inside To that Chimney can derive conversion to
+  *
+  * @since 1.5.0
+  */
 trait TotalOuterTransformer[From, To, InnerFrom, InnerTo] {
 
+  /** Converts the outer type when the conversion of inner types turns out to be total. */
   def transformWithTotalInner(
       src: From,
       inner: InnerFrom => InnerTo
   ): To
 
+  /** Converts the outer type when the conversion of inner types turns out to be partial. */
   def transformWithPartialInner(
       src: From,
       failFast: Boolean,
