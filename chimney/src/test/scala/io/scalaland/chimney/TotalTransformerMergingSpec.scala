1abd4c298fe417401d13a9d99ae6b8a9c8c2926a
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2025-01-10 15:59:17 +0100

    Test withFallback for tuples

--- chimney/src/test/scala/io/scalaland/chimney/TotalTransformerMergingSpec.scala
+++ chimney/src/test/scala/io/scalaland/chimney/TotalTransformerMergingSpec.scala
@@ -29,6 +29,43 @@ class TotalTransformerMergingSpec extends ChimneySpec {
         .transform ==> Nested(Baz(1, "3", 2.0))
     }
 
+    test("should merge cases classes (with at least 1 tuple) into case class") {
+      import merges.Disjoint.*, merges.Nested
+
+      Foo(1, "b", 3.0).into[Baz[Double]].withFallback((4, "e", 6.0)).transform ==> Baz(1, "b", 3.0, 4, "e", 6.0)
+      (1, "b", 3.0).into[Baz[Double]].withFallback(Bar(4, "e", 6.0)).transform ==> Baz(1, "b", 3.0, 4, "e", 6.0)
+
+      Nested(Foo(1, "b", 3.0))
+        .into[Nested[Baz[Double]]]
+        .withFallback(Nested((4, "e", 6.0)))
+        .transform ==> Nested(Baz(1, "b", 3.0, 4, "e", 6.0))
+      Nested((1, "b", 3.0))
+        .into[Nested[Baz[Double]]]
+        .withFallback(Nested(Bar(4, "e", 6.0)))
+        .transform ==> Nested(Baz(1, "b", 3.0, 4, "e", 6.0))
+    }
+
+    test("should merge cases classes (with at least 1 tuple) into tuple") {
+      import merges.Disjoint.*, merges.Nested
+
+      Foo(1, "b", 3.0)
+        .into[(Int, String, Double, Int, String, Double)]
+        .withFallback((4, "e", 6.0))
+        .transform ==> (1, "b", 3.0, 4, "e", 6.0)
+      (1, "b", 3.0)
+        .into[(Int, String, Double, Int, String, Double)]
+        .withFallback(Bar(4, "e", 6.0))
+        .transform ==> (1, "b", 3.0, 4, "e", 6.0)
+
+      Nested(Foo(1, "b", 3.0))
+        .into[Nested[(Int, String, Double, Int, String, Double)]]
+        .withFallback(Nested((4, "e", 6.0)))
+        .transform ==> Nested((1, "b", 3.0, 4, "e", 6.0))
+      Nested((1, "b", 3.0))
+        .into[Nested[(Int, String, Double, Int, String, Double)]]
+        .withFallback(Nested(Bar(4, "e", 6.0)))
+        .transform ==> Nested((1, "b", 3.0, 4, "e", 6.0))
+    }
   }
 
   // TODO: same for withFallbackFrom
