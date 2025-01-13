0520bc58fe053bdc40f5fdeba66d860f46115848
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2025-01-13 17:48:08 +0100

    Test Option merging with and without flags

---
+++ chimney/src/test/scala/io/scalaland/chimney/PartialMergingStdLibSpec.scala
@@ -0,0 +1,272 @@
+package io.scalaland.chimney
+
+import io.scalaland.chimney.dsl.*
+import io.scalaland.chimney.fixtures.*
+import io.scalaland.chimney.PartialTransformer
+
+class PartialMergingStdLibSpec extends ChimneySpec {
+
+  group("setting .withFallback(fallbackValue)") {
+
+    test("should use only source Option when no merging strategy is enabled") {
+      import merges.Nested
+
+      implicit val s2i: PartialTransformer[String, Int] = PartialTransformer.fromFunction[String, Int](_.toInt)
+
+      "10"
+        .intoPartial[Option[Int]]
+        .withFallback(20)
+        .transform
+        .asOption ==> Some(Some(10))
+      Option("10")
+        .intoPartial[Option[Int]]
+        .withFallback(Option(20))
+        .transform
+        .asOption ==> Some(Some(10))
+      Option("10")
+        .intoPartial[Option[Int]]
+        .withFallback(Option.empty[Int])
+        .transform
+        .asOption ==> Some(Some(10))
+      Option
+        .empty[String]
+        .intoPartial[Option[Int]]
+        .withFallback(Option(20))
+        .transform
+        .asOption ==> Some(None)
+      Option
+        .empty[String]
+        .intoPartial[Option[Int]]
+        .withFallback(Option.empty[Int])
+        .transform
+        .asOption ==> Some(None)
+
+      Nested("10")
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(20))
+        .transform
+        .asOption ==> Some(Nested(Some(10)))
+      Nested(Option("10"))
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option(20)))
+        .transform
+        .asOption ==> Some(Nested(Some(10)))
+      Nested(Option("10"))
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option.empty[String]))
+        .transform
+        .asOption ==> Some(Nested(Some(10)))
+      Nested(Option.empty[String])
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option(20)))
+        .transform
+        .asOption ==> Some(Nested(None))
+      Nested(Option.empty[String])
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option.empty[Int]))
+        .transform
+        .asOption ==> Some(Nested(None))
+    }
+  }
+
+  group("setting .withFallbackFrom(selectorFrom)(fallbackValue)") {
+
+    test("should use only source Option when no merging strategy is enabled") {
+      import merges.Nested
+
+      implicit val s2i: PartialTransformer[String, Int] = PartialTransformer.fromFunction[String, Int](_.toInt)
+
+      Nested("10")
+        .intoPartial[Nested[Option[Int]]]
+        .withFallbackFrom(_.value)(20)
+        .transform.asOption ==> Some(Nested(Some(10)))
+      Nested(Option("10"))
+        .intoPartial[Nested[Option[Int]]]
+        .withFallbackFrom(_.value)(Option(20))
+        .transform
+        .asOption ==> Some(Nested(Some(10)))
+      Nested(Option("10"))
+        .intoPartial[Nested[Option[Int]]]
+        .withFallbackFrom(_.value)(Option.empty[String])
+        .transform
+        .asOption ==> Some(Nested(Some(10)))
+      Nested(Option.empty[String])
+        .intoPartial[Nested[Option[Int]]]
+        .withFallbackFrom(_.value)(Option(20))
+        .transform
+        .asOption ==> Some(Nested(None))
+      Nested(Option.empty[String])
+        .intoPartial[Nested[Option[Int]]]
+        .withFallbackFrom(_.value)(Option.empty[Int])
+        .transform
+        .asOption ==> Some(Nested(None))
+
+      Nested(Nested("10"))
+        .intoPartial[Nested[Nested[Option[Int]]]]
+        .withFallbackFrom(_.value)(Nested(20))
+        .transform
+        .asOption ==> Some(Nested(Nested(Some(10))))
+      Nested(Nested(Option("10")))
+        .intoPartial[Nested[Nested[Option[Int]]]]
+        .withFallbackFrom(_.value)(Nested(Option(20)))
+        .transform
+        .asOption ==> Some(Nested(Nested(Some(10))))
+      Nested(Nested(Option("10")))
+        .intoPartial[Nested[Nested[Option[Int]]]]
+        .withFallbackFrom(_.value)(Nested(Option.empty[Int]))
+        .transform
+        .asOption ==> Some(Nested(Nested(Some(10))))
+      Nested(Nested(Option.empty[String]))
+        .intoPartial[Nested[Nested[Option[Int]]]]
+        .withFallbackFrom(_.value)(Nested(Option(20)))
+        .transform
+        .asOption ==> Some(Nested(Nested(None)))
+      Nested(Nested(Option.empty[String]))
+        .intoPartial[Nested[Nested[Option[Int]]]]
+        .withFallbackFrom(_.value)(Nested(Option.empty[Int]))
+        .transform
+        .asOption ==> Some(Nested(Nested(None)))
+    }
+  }
+
+  group("flag .enableOptionFallbackMerge(SourceOrElseFallback)") {
+
+    test("should merge Options from source to fallback when SourceOrElseFallback strategy is enabled") {
+      import merges.Nested
+
+      implicit val s2i: PartialTransformer[String, Int] = PartialTransformer.fromFunction[String, Int](_.toInt)
+
+      "10".intoPartial[Option[Int]]
+        .withFallback(20)
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform
+        .asOption ==> Some(Some(10))
+      Option("10")
+        .intoPartial[Option[Int]]
+        .withFallback(Option(20))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform
+        .asOption ==> Some(Some(10))
+      Option("10")
+        .intoPartial[Option[Int]]
+        .withFallback(Option.empty[String])
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform
+        .asOption ==> Some(Some(10))
+      Option
+        .empty[String]
+        .intoPartial[Option[Int]]
+        .withFallback(Option(20))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform
+        .asOption ==> Some(Some(20))
+      Option
+        .empty[String]
+        .intoPartial[Option[Int]]
+        .withFallback(Option.empty[Int])
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform
+        .asOption ==> Some(None)
+
+      Nested("10")
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(20))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform
+        .asOption ==> Some(Nested(Some(10)))
+      Nested(Option("10"))
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option(20)))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform
+        .asOption ==> Some(Nested(Some(10)))
+      Nested(Option("10"))
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option.empty[Int]))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform
+        .asOption ==> Some(Nested(Some(10)))
+      Nested(Option.empty[String])
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option(20)))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform
+        .asOption ==> Some(Nested(Some(20)))
+      Nested(Option.empty[String])
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option.empty[Int]))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform
+        .asOption ==> Some(Nested(None))
+    }
+
+    test("should merge Options from fallback to source when FallbackOrElseSource strategy is enabled") {
+      import merges.Nested
+
+      implicit val s2i: PartialTransformer[String, Int] = PartialTransformer.fromFunction[String, Int](_.toInt)
+
+      "10".intoPartial[Option[Int]]
+        .withFallback(20)
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform
+        .asOption ==> Some(Some(20))
+      Option("10")
+        .intoPartial[Option[Int]]
+        .withFallback(Option(20))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform
+        .asOption ==> Some(Some(20))
+      Option("10")
+        .intoPartial[Option[Int]]
+        .withFallback(Option.empty[Int])
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform
+        .asOption ==> Some(Some(10))
+      Option
+        .empty[String]
+        .intoPartial[Option[Int]]
+        .withFallback(Option(20))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform
+        .asOption ==> Some(Some(20))
+      Option
+        .empty[String]
+        .intoPartial[Option[Int]]
+        .withFallback(Option.empty[Int])
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform
+        .asOption ==> Some(None)
+
+      Nested("10")
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(20))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform
+        .asOption ==> Some(Nested(Some(20)))
+      Nested(Option("10"))
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option(20)))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform
+        .asOption ==> Some(Nested(Some(20)))
+      Nested(Option("10"))
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option.empty[Int]))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform
+        .asOption ==> Some(Nested(Some(10)))
+      Nested(Option.empty[String])
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option(20)))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform
+        .asOption ==> Some(Nested(Some(20)))
+      Nested(Option.empty[String])
+        .intoPartial[Nested[Option[Int]]]
+        .withFallback(Nested(Option.empty[Int]))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform
+        .asOption ==> Some(Nested(None))
+    }
+  }
+}
---
+++ chimney/src/test/scala/io/scalaland/chimney/TotalMergingStdLibSpec.scala
@@ -0,0 +1,233 @@
+package io.scalaland.chimney
+
+import io.scalaland.chimney.dsl.*
+import io.scalaland.chimney.fixtures.*
+import io.scalaland.chimney.Transformer
+
+class TotalMergingStdLibSpec extends ChimneySpec {
+
+  group("setting .withFallback(fallbackValue)") {
+
+    test("should use only source Option when no merging strategy is enabled") {
+      import merges.Nested
+
+      implicit val i2s: Transformer[Int, String] = _.toString
+
+      10
+        .into[Option[String]]
+        .withFallback("fallback")
+        .transform ==> Some("10")
+      Option(10)
+        .into[Option[String]]
+        .withFallback(Option("fallback"))
+        .transform ==> Some("10")
+      Option(10)
+        .into[Option[String]]
+        .withFallback(Option.empty[String])
+        .transform ==> Some("10")
+      Option
+        .empty[Int]
+        .into[Option[String]]
+        .withFallback(Option("fallback"))
+        .transform ==> None
+      Option
+        .empty[Int]
+        .into[Option[String]]
+        .withFallback(Option.empty[String])
+        .transform ==> None
+
+      Nested(10)
+        .into[Nested[Option[String]]]
+        .withFallback(Nested("fallback"))
+        .transform ==> Nested(Some("10"))
+      Nested(Option(10))
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option("fallback")))
+        .transform ==> Nested(Some("10"))
+      Nested(Option(10))
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option.empty[String]))
+        .transform ==> Nested(Some("10"))
+      Nested(Option.empty[Int])
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option("fallback")))
+        .transform ==> Nested(None)
+      Nested(Option.empty[Int])
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option.empty[String]))
+        .transform ==> Nested(None)
+    }
+  }
+
+  group("setting .withFallbackFrom(selectorFrom)(fallbackValue)") {
+
+    test("should use only source Option when no merging strategy is enabled") {
+      import merges.Nested
+
+      implicit val i2s: Transformer[Int, String] = _.toString
+
+      Nested(10)
+        .into[Nested[Option[String]]]
+        .withFallbackFrom(_.value)("fallback")
+        .transform ==> Nested(Some("10"))
+      Nested(Option(10))
+        .into[Nested[Option[String]]]
+        .withFallbackFrom(_.value)(Option("fallback"))
+        .transform ==> Nested(Some("10"))
+      Nested(Option(10))
+        .into[Nested[Option[String]]]
+        .withFallbackFrom(_.value)(Option.empty[String])
+        .transform ==> Nested(Some("10"))
+      Nested(Option.empty[Int])
+        .into[Nested[Option[String]]]
+        .withFallbackFrom(_.value)(Option("fallback"))
+        .transform ==> Nested(None)
+      Nested(Option.empty[Int])
+        .into[Nested[Option[String]]]
+        .withFallbackFrom(_.value)(Option.empty[String])
+        .transform ==> Nested(None)
+
+      Nested(Nested(10))
+        .into[Nested[Nested[Option[String]]]]
+        .withFallbackFrom(_.value)(Nested("fallback"))
+        .transform ==> Nested(Nested(Some("10")))
+      Nested(Nested(Option(10)))
+        .into[Nested[Nested[Option[String]]]]
+        .withFallbackFrom(_.value)(Nested(Option("fallback")))
+        .transform ==> Nested(Nested(Some("10")))
+      Nested(Nested(Option(10)))
+        .into[Nested[Nested[Option[String]]]]
+        .withFallbackFrom(_.value)(Nested(Option.empty[String]))
+        .transform ==> Nested(Nested(Some("10")))
+      Nested(Nested(Option.empty[Int]))
+        .into[Nested[Nested[Option[String]]]]
+        .withFallbackFrom(_.value)(Nested(Option("fallback")))
+        .transform ==> Nested(Nested(None))
+      Nested(Nested(Option.empty[Int]))
+        .into[Nested[Nested[Option[String]]]]
+        .withFallbackFrom(_.value)(Nested(Option.empty[String]))
+        .transform ==> Nested(Nested(None))
+    }
+  }
+
+  group("flag .enableOptionFallbackMerge(SourceOrElseFallback)") {
+
+    test("should merge Options from source to fallback when SourceOrElseFallback strategy is enabled") {
+      import merges.Nested
+
+      implicit val i2s: Transformer[Int, String] = _.toString
+
+      10.into[Option[String]]
+        .withFallback("fallback")
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform ==> Some("10")
+      Option(10)
+        .into[Option[String]]
+        .withFallback(Option("fallback"))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform ==> Some("10")
+      Option(10)
+        .into[Option[String]]
+        .withFallback(Option.empty[String])
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform ==> Some("10")
+      Option
+        .empty[Int]
+        .into[Option[String]]
+        .withFallback(Option("fallback"))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform ==> Some("fallback")
+      Option
+        .empty[Int]
+        .into[Option[String]]
+        .withFallback(Option.empty[String])
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform ==> None
+
+      Nested(10)
+        .into[Nested[Option[String]]]
+        .withFallback(Nested("fallback"))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform ==> Nested(Some("10"))
+      Nested(Option(10))
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option("fallback")))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform ==> Nested(Some("10"))
+      Nested(Option(10))
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option.empty[String]))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform ==> Nested(Some("10"))
+      Nested(Option.empty[Int])
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option("fallback")))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform ==> Nested(Some("fallback"))
+      Nested(Option.empty[Int])
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option.empty[String]))
+        .enableOptionFallbackMerge(SourceOrElseFallback)
+        .transform ==> Nested(None)
+    }
+
+    test("should merge Options from fallback to source when FallbackOrElseSource strategy is enabled") {
+      import merges.Nested
+
+      implicit val i2s: Transformer[Int, String] = _.toString
+
+      10.into[Option[String]]
+        .withFallback("fallback")
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform ==> Some("fallback")
+      Option(10)
+        .into[Option[String]]
+        .withFallback(Option("fallback"))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform ==> Some("fallback")
+      Option(10)
+        .into[Option[String]]
+        .withFallback(Option.empty[String])
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform ==> Some("10")
+      Option
+        .empty[Int]
+        .into[Option[String]]
+        .withFallback(Option("fallback"))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform ==> Some("fallback")
+      Option
+        .empty[Int]
+        .into[Option[String]]
+        .withFallback(Option.empty[String])
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform ==> None
+
+      Nested(10)
+        .into[Nested[Option[String]]]
+        .withFallback(Nested("fallback"))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform ==> Nested(Some("fallback"))
+      Nested(Option(10))
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option("fallback")))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform ==> Nested(Some("fallback"))
+      Nested(Option(10))
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option.empty[String]))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform ==> Nested(Some("10"))
+      Nested(Option.empty[Int])
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option("fallback")))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform ==> Nested(Some("fallback"))
+      Nested(Option.empty[Int])
+        .into[Nested[Option[String]]]
+        .withFallback(Nested(Option.empty[String]))
+        .enableOptionFallbackMerge(FallbackOrElseSource)
+        .transform ==> Nested(None)
+    }
+  }
+}
