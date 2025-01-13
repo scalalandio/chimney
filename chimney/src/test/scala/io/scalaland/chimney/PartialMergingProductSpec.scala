a566006d298bf6df28acd516a1f06dfb2c9a430f
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2025-01-13 17:48:08 +0100

    Test Option merging with and without flags

--- chimney/src/test/scala/io/scalaland/chimney/PartialMergingProductSpec.scala
+++ chimney/src/test/scala/io/scalaland/chimney/PartialMergingProductSpec.scala
@@ -3,82 +3,108 @@ package io.scalaland.chimney
 import io.scalaland.chimney.dsl.*
 import io.scalaland.chimney.fixtures.*
 
-class TotalTransformerMergingSpec extends ChimneySpec {
+class PartialMergingProductSpec extends ChimneySpec {
 
   group("setting .withFallback(fallbackValue)") {
 
     test("should merge 2 case classes with disjoint field names") {
       import merges.Disjoint.*, merges.Nested
 
-      Foo(1, "b", 3.0).into[Baz[Double]].withFallback(Bar(4, "e", 6.0)).transform ==> Baz(1, "b", 3.0, 4, "e", 6.0)
+      Foo(1, "b", 3.0)
+        .intoPartial[Baz[Double]]
+        .withFallback(Bar(4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Baz(1, "b", 3.0, 4, "e", 6.0))
 
       Nested(Foo(1, "b", 3.0))
-        .into[Nested[Baz[Double]]]
+        .intoPartial[Nested[Baz[Double]]]
         .withFallback(Nested(Bar(4, "e", 6.0)))
-        .transform ==> Nested(Baz(1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested(Baz(1, "b", 3.0, 4, "e", 6.0)))
     }
 
     test("should merge 2 case classes with overlapping field names") {
       import merges.Overlapping.*, merges.Nested
 
-      Foo(1, 2.0).into[Baz[Double]].withFallback(Bar("3", 4.0)).transform ==> Baz(1, "3", 2.0)
+      Foo(1, 2.0)
+        .intoPartial[Baz[Double]]
+        .withFallback(Bar("3", 4.0))
+        .transform
+        .asOption ==> Some(Baz(1, "3", 2.0))
 
       Nested(Foo(1, 2.0))
-        .into[Nested[Baz[Double]]]
+        .intoPartial[Nested[Baz[Double]]]
         .withFallback(Nested(Bar("3", 4.0)))
-        .transform ==> Nested(Baz(1, "3", 2.0))
+        .transform
+        .asOption ==> Some(Nested(Baz(1, "3", 2.0)))
     }
 
     test("should merge cases classes (with at least 1 tuple) into case class") {
       import merges.Disjoint.*, merges.Nested
 
-      Foo(1, "b", 3.0).into[Baz[Double]].withFallback((4, "e", 6.0)).transform ==> Baz(1, "b", 3.0, 4, "e", 6.0)
-      (1, "b", 3.0).into[Baz[Double]].withFallback(Bar(4, "e", 6.0)).transform ==> Baz(1, "b", 3.0, 4, "e", 6.0)
+      Foo(1, "b", 3.0)
+        .intoPartial[Baz[Double]]
+        .withFallback((4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Baz(1, "b", 3.0, 4, "e", 6.0))
+      (1, "b", 3.0)
+        .intoPartial[Baz[Double]]
+        .withFallback(Bar(4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Baz(1, "b", 3.0, 4, "e", 6.0))
 
       Nested(Foo(1, "b", 3.0))
-        .into[Nested[Baz[Double]]]
+        .intoPartial[Nested[Baz[Double]]]
         .withFallback(Nested((4, "e", 6.0)))
-        .transform ==> Nested(Baz(1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested(Baz(1, "b", 3.0, 4, "e", 6.0)))
       Nested((1, "b", 3.0))
-        .into[Nested[Baz[Double]]]
+        .intoPartial[Nested[Baz[Double]]]
         .withFallback(Nested(Bar(4, "e", 6.0)))
-        .transform ==> Nested(Baz(1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested(Baz(1, "b", 3.0, 4, "e", 6.0)))
     }
 
     test("should merge cases classes (with at least 1 tuple) into tuple") {
       import merges.Disjoint.*, merges.Nested
 
       Foo(1, "b", 3.0)
-        .into[(Int, String, Double, Int, String, Double)]
+        .intoPartial[(Int, String, Double, Int, String, Double)]
         .withFallback((4, "e", 6.0))
-        .transform ==> ((1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some((1, "b", 3.0, 4, "e", 6.0))
       (1, "b", 3.0)
-        .into[(Int, String, Double, Int, String, Double)]
+        .intoPartial[(Int, String, Double, Int, String, Double)]
         .withFallback(Bar(4, "e", 6.0))
-        .transform ==> ((1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some((1, "b", 3.0, 4, "e", 6.0))
 
       Nested(Foo(1, "b", 3.0))
-        .into[Nested[(Int, String, Double, Int, String, Double)]]
+        .intoPartial[Nested[(Int, String, Double, Int, String, Double)]]
         .withFallback(Nested((4, "e", 6.0)))
-        .transform ==> Nested((1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested((1, "b", 3.0, 4, "e", 6.0)))
       Nested((1, "b", 3.0))
-        .into[Nested[(Int, String, Double, Int, String, Double)]]
+        .intoPartial[Nested[(Int, String, Double, Int, String, Double)]]
         .withFallback(Nested(Bar(4, "e", 6.0)))
-        .transform ==> Nested((1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested((1, "b", 3.0, 4, "e", 6.0)))
     }
 
     test("should merge cases classes (none of them a tuple) into tuple") {
       import merges.Disjoint.*, merges.Nested
 
       Foo(1, "b", 3.0)
-        .into[(Int, String, Double, Int, String, Double)]
+        .intoPartial[(Int, String, Double, Int, String, Double)]
         .withFallback(Bar(4, "e", 6.0))
-        .transform ==> ((1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some((1, "b", 3.0, 4, "e", 6.0))
 
       Nested(Foo(1, "b", 3.0))
-        .into[Nested[(Int, String, Double, Int, String, Double)]]
+        .intoPartial[Nested[(Int, String, Double, Int, String, Double)]]
         .withFallback(Nested(Bar(4, "e", 6.0)))
-        .transform ==> Nested((1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested((1, "b", 3.0, 4, "e", 6.0)))
     }
 
     test("should merge cases classes (none of them a tuple) into tuple") {
@@ -87,7 +113,7 @@ class TotalTransformerMergingSpec extends ChimneySpec {
       compileErrors(
         """
         Foo(1, "b", 3.0)
-          .into[(Int, String, Double, Int, String, Double, Long)]
+          .intoPartial[(Int, String, Double, Int, String, Double, Long)]
           .withFallback(Bar(4, "e", 6.0))
           .transform
         """
@@ -101,7 +127,7 @@ class TotalTransformerMergingSpec extends ChimneySpec {
       compileErrors(
         """
         Nested(Foo(1, "b", 3.0))
-          .into[Nested[(Int, String, Double, Int, String, Double, Long)]]
+          .intoPartial[Nested[(Int, String, Double, Int, String, Double, Long)]]
           .withFallback(Nested(Bar(4, "e", 6.0)))
           .transform
         """
@@ -119,20 +145,22 @@ class TotalTransformerMergingSpec extends ChimneySpec {
       import merges.Disjoint.*, merges.Nested
 
       Foo(1, "b", 3.0)
-        .into[(Int, String, Double, Int, String, Double, Int)]
+        .intoPartial[(Int, String, Double, Int, String, Double, Int)]
         .withFallback(Bar(4, "e", 6.0))
         .withFieldConst(_._2, "const")
         .withFieldComputed(_._5, _ => "computed")
         .withFieldRenamed(_.a, _._7)
-        .transform ==> ((1, "const", 3.0, 4, "computed", 6.0, 1))
+        .transform
+        .asOption ==> Some((1, "const", 3.0, 4, "computed", 6.0, 1))
 
       Nested(Foo(1, "b", 3.0))
-        .into[Nested[(Int, String, Double, Int, String, Double, Int)]]
+        .intoPartial[Nested[(Int, String, Double, Int, String, Double, Int)]]
         .withFallback(Nested(Bar(4, "e", 6.0)))
         .withFieldConst(_.value._2, "const")
         .withFieldComputed(_.value._5, _ => "computed")
         .withFieldRenamed(_.value.a, _.value._7)
-        .transform ==> Nested((1, "const", 3.0, 4, "computed", 6.0, 1))
+        .transform
+        .asOption ==> Some(Nested((1, "const", 3.0, 4, "computed", 6.0, 1)))
     }
   }
 
@@ -142,86 +170,100 @@ class TotalTransformerMergingSpec extends ChimneySpec {
       import merges.Disjoint.*, merges.Nested
 
       Nested(Foo(1, "b", 3.0))
-        .into[Nested[Baz[Double]]]
+        .intoPartial[Nested[Baz[Double]]]
         .withFallbackFrom(_.value)(Bar(4, "e", 6.0))
-        .transform ==> Nested(Baz(1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested(Baz(1, "b", 3.0, 4, "e", 6.0)))
 
       Nested(Nested(Foo(1, "b", 3.0)))
-        .into[Nested[Nested[Baz[Double]]]]
+        .intoPartial[Nested[Nested[Baz[Double]]]]
         .withFallbackFrom(_.value)(Nested(Bar(4, "e", 6.0)))
-        .transform ==> Nested(Nested(Baz(1, "b", 3.0, 4, "e", 6.0)))
+        .transform
+        .asOption ==> Some(Nested(Nested(Baz(1, "b", 3.0, 4, "e", 6.0))))
     }
 
     test("should merge 2 case classes with overlapping field names") {
       import merges.Overlapping.*, merges.Nested
 
       Nested(Foo(1, 2.0))
-        .into[Nested[Baz[Double]]]
+        .intoPartial[Nested[Baz[Double]]]
         .withFallbackFrom(_.value)(Bar("3", 4.0))
-        .transform ==> Nested(Baz(1, "3", 2.0))
+        .transform
+        .asOption ==> Some(Nested(Baz(1, "3", 2.0)))
 
       Nested(Nested(Foo(1, 2.0)))
-        .into[Nested[Nested[Baz[Double]]]]
+        .intoPartial[Nested[Nested[Baz[Double]]]]
         .withFallbackFrom(_.value)(Nested(Bar("3", 4.0)))
-        .transform ==> Nested(Nested(Baz(1, "3", 2.0)))
+        .transform
+        .asOption ==> Some(Nested(Nested(Baz(1, "3", 2.0))))
     }
 
     test("should merge cases classes (with at least 1 tuple) into case class") {
       import merges.Disjoint.*, merges.Nested
 
       Nested(Foo(1, "b", 3.0))
-        .into[Nested[Baz[Double]]]
+        .intoPartial[Nested[Baz[Double]]]
         .withFallbackFrom(_.value)((4, "e", 6.0))
-        .transform ==> Nested(Baz(1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested(Baz(1, "b", 3.0, 4, "e", 6.0)))
       Nested((1, "b", 3.0))
-        .into[Nested[Baz[Double]]]
+        .intoPartial[Nested[Baz[Double]]]
         .withFallbackFrom(_.value)(Bar(4, "e", 6.0))
-        .transform ==> Nested(Baz(1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested(Baz(1, "b", 3.0, 4, "e", 6.0)))
 
       Nested(Nested(Foo(1, "b", 3.0)))
-        .into[Nested[Nested[Baz[Double]]]]
+        .intoPartial[Nested[Nested[Baz[Double]]]]
         .withFallbackFrom(_.value)(Nested((4, "e", 6.0)))
-        .transform ==> Nested(Nested(Baz(1, "b", 3.0, 4, "e", 6.0)))
+        .transform
+        .asOption ==> Some(Nested(Nested(Baz(1, "b", 3.0, 4, "e", 6.0))))
       Nested(Nested((1, "b", 3.0)))
-        .into[Nested[Nested[Baz[Double]]]]
+        .intoPartial[Nested[Nested[Baz[Double]]]]
         .withFallbackFrom(_.value)(Nested(Bar(4, "e", 6.0)))
-        .transform ==> Nested(Nested(Baz(1, "b", 3.0, 4, "e", 6.0)))
+        .transform
+        .asOption ==> Some(Nested(Nested(Baz(1, "b", 3.0, 4, "e", 6.0))))
     }
 
     test("should merge cases classes (with at least 1 tuple) into tuple") {
       import merges.Disjoint.*, merges.Nested
 
       Nested(Foo(1, "b", 3.0))
-        .into[Nested[(Int, String, Double, Int, String, Double)]]
+        .intoPartial[Nested[(Int, String, Double, Int, String, Double)]]
         .withFallbackFrom(_.value)((4, "e", 6.0))
-        .transform ==> Nested((1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested((1, "b", 3.0, 4, "e", 6.0)))
       Nested((1, "b", 3.0))
-        .into[Nested[(Int, String, Double, Int, String, Double)]]
+        .intoPartial[Nested[(Int, String, Double, Int, String, Double)]]
         .withFallbackFrom(_.value)(Bar(4, "e", 6.0))
-        .transform ==> Nested((1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested((1, "b", 3.0, 4, "e", 6.0)))
 
       Nested(Nested(Foo(1, "b", 3.0)))
-        .into[Nested[Nested[(Int, String, Double, Int, String, Double)]]]
+        .intoPartial[Nested[Nested[(Int, String, Double, Int, String, Double)]]]
         .withFallbackFrom(_.value)(Nested((4, "e", 6.0)))
-        .transform ==> Nested(Nested((1, "b", 3.0, 4, "e", 6.0)))
+        .transform
+        .asOption ==> Some(Nested(Nested((1, "b", 3.0, 4, "e", 6.0))))
       Nested(Nested((1, "b", 3.0)))
-        .into[Nested[Nested[(Int, String, Double, Int, String, Double)]]]
+        .intoPartial[Nested[Nested[(Int, String, Double, Int, String, Double)]]]
         .withFallbackFrom(_.value)(Nested(Bar(4, "e", 6.0)))
-        .transform ==> Nested(Nested((1, "b", 3.0, 4, "e", 6.0)))
+        .transform
+        .asOption ==> Some(Nested(Nested((1, "b", 3.0, 4, "e", 6.0))))
     }
 
     test("should merge cases classes (none of them a tuple) into tuple") {
       import merges.Disjoint.*, merges.Nested
 
       Nested(Foo(1, "b", 3.0))
-        .into[Nested[(Int, String, Double, Int, String, Double)]]
+        .intoPartial[Nested[(Int, String, Double, Int, String, Double)]]
         .withFallbackFrom(_.value)(Bar(4, "e", 6.0))
-        .transform ==> Nested((1, "b", 3.0, 4, "e", 6.0))
+        .transform
+        .asOption ==> Some(Nested((1, "b", 3.0, 4, "e", 6.0)))
 
       Nested(Nested(Foo(1, "b", 3.0)))
-        .into[Nested[Nested[(Int, String, Double, Int, String, Double)]]]
+        .intoPartial[Nested[Nested[(Int, String, Double, Int, String, Double)]]]
         .withFallbackFrom(_.value)(Nested(Bar(4, "e", 6.0)))
-        .transform ==> Nested(Nested((1, "b", 3.0, 4, "e", 6.0)))
+        .transform
+        .asOption ==> Some(Nested(Nested((1, "b", 3.0, 4, "e", 6.0))))
     }
 
     test("should merge cases classes (none of them a tuple) into tuple") {
@@ -230,7 +272,7 @@ class TotalTransformerMergingSpec extends ChimneySpec {
       compileErrors(
         """
         Nested(Foo(1, "b", 3.0))
-          .into[Nested[(Int, String, Double, Int, String, Double, Long)]]
+          .intoPartial[Nested[(Int, String, Double, Int, String, Double, Long)]]
           .withFallbackFrom(_.value)(Bar(4, "e", 6.0))
           .transform
         """
@@ -246,7 +288,7 @@ class TotalTransformerMergingSpec extends ChimneySpec {
       compileErrors(
         """
         Nested(Nested(Foo(1, "b", 3.0)))
-          .into[Nested[Nested[(Int, String, Double, Int, String, Double, Long)]]]
+          .intoPartial[Nested[Nested[(Int, String, Double, Int, String, Double, Long)]]]
           .withFallbackFrom(_.value)(Nested(Bar(4, "e", 6.0)))
           .transform
         """
@@ -266,20 +308,22 @@ class TotalTransformerMergingSpec extends ChimneySpec {
       import merges.Disjoint.*, merges.Nested
 
       Nested(Foo(1, "b", 3.0))
-        .into[Nested[(Int, String, Double, Int, String, Double, Int)]]
+        .intoPartial[Nested[(Int, String, Double, Int, String, Double, Int)]]
         .withFallbackFrom(_.value)(Bar(4, "e", 6.0))
         .withFieldConst(_.value._2, "const")
         .withFieldComputed(_.value._5, _ => "computed")
         .withFieldRenamed(_.value.a, _.value._7)
-        .transform ==> Nested((1, "const", 3.0, 4, "computed", 6.0, 1))
+        .transform
+        .asOption ==> Some(Nested((1, "const", 3.0, 4, "computed", 6.0, 1)))
 
       Nested(Nested(Foo(1, "b", 3.0)))
-        .into[Nested[Nested[(Int, String, Double, Int, String, Double, Int)]]]
+        .intoPartial[Nested[Nested[(Int, String, Double, Int, String, Double, Int)]]]
         .withFallbackFrom(_.value)(Nested(Bar(4, "e", 6.0)))
         .withFieldConst(_.value.value._2, "const")
         .withFieldComputedFrom(_.value)(_.value.value._5, _ => "computed")
         .withFieldRenamed(_.value.value.a, _.value.value._7)
-        .transform ==> Nested(Nested((1, "const", 3.0, 4, "computed", 6.0, 1)))
+        .transform
+        .asOption ==> Some(Nested(Nested((1, "const", 3.0, 4, "computed", 6.0, 1))))
     }
   }
 }
