6f20ef34f8c033eb7faaae0e47e92c50f45f4565
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2025-01-22 17:43:10 +0100

    Add scaladoc and links to merge sections for options/eithers/collections

--- chimney/src/main/scala/io/scalaland/chimney/dsl/CollectionFallbackMergeStrategy.scala
+++ chimney/src/main/scala/io/scalaland/chimney/dsl/CollectionFallbackMergeStrategy.scala
@@ -1,6 +1,33 @@
 package io.scalaland.chimney.dsl
 
-// TODO: documentation
+/** What should happen when both source value and fallback values are collections.
+  *
+  * @see
+  *   [[https://chimney.readthedocs.io/supported-transformations/#merging-collection-with-collection-into-collection]]
+  *   for more details
+  *
+  * @since 1.7.0
+  */
 sealed abstract class CollectionFallbackMergeStrategy
+
+/** When both source value and fallback values are collections they should be combined using
+  * `src ++ fallback1 ++ fallback2 ...`.
+  *
+  * @see
+  *   [[https://chimney.readthedocs.io/supported-transformations/#merging-collection-with-collection-into-collection]]
+  *   for more details
+  *
+  * @since 1.7.0
+  */
 case object SourceAppendFallback extends CollectionFallbackMergeStrategy
+
+/** When both source value and fallback values are collections they should be combined using
+  * `fallback2 ++ fallback1 ++ src`.
+  *
+  * @see
+  *   [[https://chimney.readthedocs.io/supported-transformations/#merging-collection-with-collection-into-collection]]
+  *   for more details
+  *
+  * @since 1.7.0
+  */
 case object FallbackAppendSource extends CollectionFallbackMergeStrategy
--- chimney/src/main/scala/io/scalaland/chimney/dsl/OptionFallbackMergeStrategy.scala
+++ chimney/src/main/scala/io/scalaland/chimney/dsl/OptionFallbackMergeStrategy.scala
@@ -1,6 +1,42 @@
 package io.scalaland.chimney.dsl
 
-// TODO: documentation
+/** What should happen when both source value and fallback values are of the Option/Either type.
+  *
+  * @see
+  *   [[https://chimney.readthedocs.io/supported-transformations/#merging-option-with-option-into-option]] for more
+  *   details
+  * @see
+  *   [[https://chimney.readthedocs.io/supported-transformations/#merging-either-with-either-into-either]] for more
+  *   details
+  *
+  * @since 1.7.0
+  */
 sealed abstract class OptionFallbackMergeStrategy
+
+/** When both source value and fallback values are of the Option/Either type they should be combined using
+  * `src.orElse(fallback1).orElse(fallback2)...`.
+  *
+  * @see
+  *   [[https://chimney.readthedocs.io/supported-transformations/#merging-option-with-option-into-option]] for more
+  *   details
+  * @see
+  *   [[https://chimney.readthedocs.io/supported-transformations/#merging-either-with-either-into-either]] for more
+  *   details
+  *
+  * @since 1.7.0
+  */
 case object SourceOrElseFallback extends OptionFallbackMergeStrategy
+
+/** When both source value and fallback values are of the Option/Either type they should be combined using
+  * `fallbck2.orElse(fallback1).orElse(src)`.
+  *
+  * @see
+  *   [[https://chimney.readthedocs.io/supported-transformations/#merging-option-with-option-into-option]] for more
+  *   details
+  * @see
+  *   [[https://chimney.readthedocs.io/supported-transformations/#merging-either-with-either-into-either]] for more
+  *   details
+  *
+  * @since 1.7.0
+  */
 case object FallbackOrElseSource extends OptionFallbackMergeStrategy
