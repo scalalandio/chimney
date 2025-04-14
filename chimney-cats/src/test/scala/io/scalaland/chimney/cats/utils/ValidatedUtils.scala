87ad50a0ab57e71b986968efce374473a3d3662f
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2025-04-14 09:28:59 +0200

    Fix cats-effect build

--- chimney-cats/src/test/scala/io/scalaland/chimney/cats/utils/ValidatedUtils.scala
+++ chimney-cats/src/test/scala/io/scalaland/chimney/cats/utils/ValidatedUtils.scala
@@ -25,10 +25,10 @@ object ValidatedUtils {
       }
 
     def toValidatedNec(err: => String): ValidatedNec[String, A] =
-      toValidated[NonEmptyChain](err)(implicitly)
+      toValidated[NonEmptyChain](err)(using InvariantMonoidal[NonEmptyChain])
 
     def toValidatedNel(err: => String): ValidatedNel[String, A] =
-      toValidated[NonEmptyList](err)(implicitly)
+      toValidated[NonEmptyList](err)(using InvariantMonoidal[NonEmptyList])
 
     def toIor[EE[_]: InvariantMonoidal](err: => String): Ior[EE[String], A] =
       opt match {
@@ -37,10 +37,10 @@ object ValidatedUtils {
       }
 
     def toIorNec(err: => String): IorNec[String, A] =
-      toIor[NonEmptyChain](err)(implicitly)
+      toIor[NonEmptyChain](err)(using InvariantMonoidal[NonEmptyChain])
 
     def toIorNel(err: => String): IorNel[String, A] =
-      toIor[NonEmptyList](err)(implicitly)
+      toIor[NonEmptyList](err)(using InvariantMonoidal[NonEmptyList])
 
     def toIorNes(err: => String): IorNes[String, A] =
       opt match {
