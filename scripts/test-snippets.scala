7b1dcf901385713f7346eced5ee49ede5f9e1fc3
Scala Steward <scala_steward@virtuslab.com>
2025-10-03 18:25:44 +0000

    Update scala-yaml to 0.3.1

--- scripts/test-snippets.scala
+++ scripts/test-snippets.scala
@@ -1,6 +1,6 @@
 //> using scala 3.3.5
 //> using dep com.kubuszok::scala-cli-md-spec:0.1.1
-//> using dep org.virtuslab::scala-yaml:0.3.0
+//> using dep org.virtuslab::scala-yaml:0.3.1
 
 import com.kubuszok.scalaclimdspec.*
 import java.io.File
